d0b60b6dd36abe7a42b74debcb71845c0e209066
Meghan <Meg.Woodford@IntelliTect.com>
2024-04-04 15:56:50 -0700

    Add vuetify & set up ref

--- wordle-web/app.vue
+++ wordle-web/app.vue
@@ -1,16 +1,41 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <v-card class="mx-auto my-8" elevation="16" max-width="344">
+        <v-card-item>
+          <v-card-title> Wordle </v-card-title>
+          <v-card-subtitle> This is our super basic wordle game </v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          {{ myText }}
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn color="pink" variant="elevated" elevation="8" @click="click()">
+            Click Me!
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+import { Game } from "./scripts/game";
+const game: Game = new Game("JUMBO");
 
+game.guess("JUMBO");
 
-  import { Game } from './scripts/game';
-  const game: Game = new Game("JUMBO");
+console.log(game);
 
-  game.guess("JUMBO");
+const myText = ref("Hello World");
 
-  console.log(game);
+function click() {
+  myText.value = "Goodbye World";
+  window.alert("Hello World");
+}
 </script>
