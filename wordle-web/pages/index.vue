72c74801a41a4507d6f09c2fc39e6da2c6296a79
Joshua Lester <jlester3@ewu.edu>
2024-04-05 10:41:18 -0700

    Added index.vue and link to game.vue

---
+++ wordle-web/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <NuxtLayout>
+        <v-card>
+            <v-card-title>Hello Welcome to Test</v-card-title>
+        </v-card>
+    </NuxtLayout>
+    <NuxtLink to="/game">Home page</NuxtLink>>
+</template>
