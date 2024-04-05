163d8669242f26d4b91b116322819fc279e63c58
Joshua Lester <jlester3@ewu.edu>
2024-04-05 12:50:43 -0700

    connected hurdle.ts to ui

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -6,9 +6,44 @@
             Hurdle it. I dare you.
           </v-card-subtitle>
         </v-card-item>
-        <NuxtLink to="/" external>
-            Home page
-        </NuxtLink>
+        
+        <v-text-field
+        v-model="input"
+        label="Will you hurdle? (y/n):"
+        outlined
+        clearable
+        />
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="blue"
+            variant="elevated"
+            elevation="8"
+            @click="enterInput()"
+          >
+            Enter
+          </v-btn>
+        </v-card-actions>
+        <v-card-text>
+            {{ output }}
+        </v-card-text>
+        <v-card-text>
+            <NuxtLink to="/" external>
+                Home page
+            </NuxtLink>
+        </v-card-text>
     </v-card>
     
 </template>
+
+<script setup lang="ts">
+import { hurdleInput } from '~/scripts/hurdle';
+
+const input = ref("");
+const output = ref("");
+
+function enterInput() {
+    output.value = hurdleInput(input.value);
+}
+
+</script>
--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,5 +1,15 @@
 <template>
-    <NuxtLink to="/game">
-        Game
-    </NuxtLink>
+    <v-card>
+        <v-card-item>
+          <v-card-title> Welcome to this beautiful website! </v-card-title>
+          <v-card-subtitle>
+            Click the game button... if you dare...
+          </v-card-subtitle>
+        </v-card-item>
+        <v-card-text>
+            <NuxtLink to="/game">
+                Game
+            </NuxtLink>
+        </v-card-text>
+    </v-card>
 </template>
