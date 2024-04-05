7bebbf6e393d13d11e128369f70c849411f99ed6
Joshua Lester <jlester3@ewu.edu>
2024-04-05 11:14:58 -0700

    Added vuetify card

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,5 +1,14 @@
 <template>
-    <NuxtLink to="/" external>
-        Home page
-    </NuxtLink>
+    <v-card class="mx-auto my-8" elevation="16" max-width="344">
+        <v-card-item>
+          <v-card-title> Hurdle Over This Wordle!!! </v-card-title>
+          <v-card-subtitle>
+            Hurdle it. I dare you.
+          </v-card-subtitle>
+        </v-card-item>
+        <NuxtLink to="/" external>
+            Home page
+        </NuxtLink>
+    </v-card>
+    
 </template>
