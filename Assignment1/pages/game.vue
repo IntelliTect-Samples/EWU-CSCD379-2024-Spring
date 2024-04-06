566bd8c5f87e7e760acc80afa16f34cbe0d2ed59
madams41 <madams41@ewu.edu>
2024-04-06 00:27:59 -0700

    added buttion to game to home

--- Assignment1/pages/game.vue
+++ Assignment1/pages/game.vue
@@ -2,4 +2,15 @@
     <v-card>
         <v-card-title>Pentagram</v-card-title>
     </v-card>
+    <NuxtLayout>
+      <v-app>
+        <v-btn @click="router.push('/')">Home</v-btn>
+        <NuxtPage />
+      </v-app>
+    </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+  const router = useRouter()
+  
+  </script>
