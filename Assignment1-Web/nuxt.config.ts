0837fa6978547b8dffb7663fa9f3c2e6914fe8c3
Alexa <adarrington@ewu.edu>
2024-04-05 20:35:00 -0700

    Added vuetify

--- Assignment1-Web/nuxt.config.ts
+++ Assignment1-Web/nuxt.config.ts
@@ -1,8 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['vuetify'],
+  },
   devtools: { enabled: true },
   modules: [
-    '@nuxt/test-utils/module'
-  ]
+    '@nuxt/test-utils/module',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
 
