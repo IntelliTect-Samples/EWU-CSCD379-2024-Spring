2a9ddc962ab2dc17d2add0d442e869a4a5563a34
Alexa <adarrington@ewu.edu>
2024-04-05 18:14:46 -0700

    Added Wordle Game and scripts

---
+++ Assignment1-Web/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+    <v-app>
+      <v-btn @click="router.push('/')">Home</v-btn>
+      <v-btn @click="router.push('/test')">Test</v-btn>
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+const router = useRouter()
+
+</script>
