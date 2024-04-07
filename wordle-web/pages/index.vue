a2b8a4cf10c51391d5278e9b45049508432bddd7
Rahat <rbhatia@ewu.edu>
2024-04-06 22:56:02 -0700

    index page

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,14 +1,17 @@
-
 <template>
-  <v-card>
-    <v-card-item>
-      <v-card-title> Welcome to this beautiful website! </v-card-title>
-      <v-card-subtitle>
-        Click the game tton... if you dare...
-      </v-card-subtitle>
-    </v-card-item>
-    <v-card-text>
-      <NuxtLink to="/game"> Game </NuxtLink>
-    </v-card-text>
-  </v-card>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title class="text-h5">Welcome to the Happy Part of Software Land!</v-card-title>
+          <v-card-text>
+            Ready to have some fun? Click the button!!
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" to="/game">No Software Developrs Allowed</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
