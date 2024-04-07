49ddfe6335b86f6e3f63d8e69755205f43d28bce
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-04-07 14:28:59 -0700

    made game title bold within app bar

--- wordle-web/layouts/default.vue
+++ wordle-web/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar color="tertiary text-primary">
-      <v-app-bar-title>Pink Wordle</v-app-bar-title>
+      <v-app-bar-title class="font-weight-bold">Pink Wordle</v-app-bar-title>
       <v-btn to="/">Home</v-btn>
       <v-btn to="/game">Game</v-btn>
     </v-app-bar>
