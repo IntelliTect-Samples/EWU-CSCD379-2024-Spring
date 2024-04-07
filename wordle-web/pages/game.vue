593616f66cfa9cc9137772b9baec2722101f7560
Rahat <rbhatia@ewu.edu>
2024-04-06 22:48:40 -0700

    game.vue

---
+++ wordle-web/pages/game.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-card class="mx-auto my-8" elevation="16" max-width="344">
+    <v-card-item>
+      <v-card-title> Let's Laugh!!! </v-card-title>
+      <v-card-subtitle> Want to laugh? I am here </v-card-subtitle>
+    </v-card-item>
+
+    <v-text-field
+      v-if="!jokeAsked"
+      v-model="input"
+      label="Would you want to light up your day? obviously :) (y/n):"
+      outlined
+      clearable />
+    <v-text-field
+      v-else
+      v-model="punchline"
+      label="Why don't scientists trust atoms?"
+      outlined
+      clearable />
+
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+          color="blue"
+          variant="elevated"
+          elevation="8"
+          @click="jokeAsked ? checkPunchline() : askForJoke()">
+          Enter
+      </v-btn>
+
+    </v-card-actions>
+    <v-card-text>
+      {{ output }}
+    </v-card-text>
+    <v-card-text>
+      <NuxtLink to="/" external> Home page </NuxtLink>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { tellJoke as tellJokeFunction } from '../scripts/joke'; // Adjust the import path as necessary and rename the imported function
+
+const input = ref('');
+const punchline = ref('');
+const output = ref('');
+const jokeAsked = ref(false);
+
+function askForJoke() {
+if (input.value.trim().toLowerCase() === 'y') {
+  jokeAsked.value = true;
+  output.value = "Great! Let's hear a joke. Why don't scientists trust atoms?";
+} else {
+  output.value = "Okay, maybe next time!";
+}
+}
+
+function checkPunchline() {
+output.value = tellJokeFunction(punchline.value); // Use the renamed imported function
+}
+</script>
--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,56 +1,14 @@
+
 <template>
-    <v-card>
-        <v-card-title>Hello Welcome to Home</v-card-title>
-    </v-card>
-
-    <v-card class="mx-auto my-8" elevation="16" max-width="344">
-        <v-card-item>
-          <v-card-title> Wordle </v-card-title>
-          <v-card-subtitle>
-            This is our super basic wordle game
-          </v-card-subtitle>
-        </v-card-item>
-
-        <v-card-text>
-          Hint: {{ game.wordToGuess }}
-          <br />
-          My Guess: {{ myGuess }}
-          <v-text-field
-            v-model="myGuess"
-            label="Enter your guess"
-            outlined
-            clearable
-          />
-        </v-card-text>
-
-        <v-card-text>
-          <div v-for="(guess, i) of game.guesses" :key="i">
-            Guess: {{ guess.letters.map(x => x.char).join("") }}
-          </div>
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="pink"
-            variant="elevated"
-            elevation="8"
-            @click="submitGuess()"
-          >
-            Click Me!
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+  <v-card>
+    <v-card-item>
+      <v-card-title> Welcome to this beautiful website! </v-card-title>
+      <v-card-subtitle>
+        Click the game tton... if you dare...
+      </v-card-subtitle>
+    </v-card-item>
+    <v-card-text>
+      <NuxtLink to="/game"> Game </NuxtLink>
+    </v-card-text>
+  </v-card>
 </template>
-
-<script setup lang="ts">
-import { Game } from "../scripts/game";
-const game: Game = reactive(new Game("JUMBO"));
-
-const myGuess = ref("");
-
-function submitGuess() {
-  game.guess(myGuess.value.toUpperCase());
-  myGuess.value = "";
-}
-</script>
