466ab6a80af88901e4b76abea439cc319bf213b7
Hadi <amallah@ewu.edu>
2024-04-07 03:31:02 -0700

    Changes

--- website/app.vue
+++ website/app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <v-btn @click="router.push('/')">Home</v-btn>
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const router = useRouter()
+
+</script>
+
---
+++ website/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-card>
+        <v-card-title>Hello Welcome to Home</v-card-title>
+    </v-card>
+
+    <v-card class="mx-auto my-8" elevation="16" max-width="344">
+        <v-card-item>
+          <v-card-title> Website </v-card-title>
+          <v-card-subtitle>
+            This is our super basic website
+          </v-card-subtitle>
+        </v-card-item>
+
+        <v-card-text>
+          <br />
+          <v-text-field
+            label="Enter your guess"
+            outlined
+            clearable
+          />
+        </v-card-text>
+    </v-card>
+</template>
