8ebc655bd9dbf85d9f9f98cc6d66156033d27fd2
conner hanson <connerhanson0826@gmail.com>
2024-04-07 20:35:11 -0700

    made second test

--- wordle-web/tests/game.test.ts
+++ wordle-web/tests/game.test.ts
@@ -1,7 +1,13 @@
 import { expect, test } from "vitest";
 import { tellJoke } from "~/Assignment1_CSCD379/EWU-CSCD379-2024-Spring/wordle-web/scripts/joke.ts";
 
-test('returns "Correct! 😄" if the punchline is correct', () => {
-  const punchline = 'because they make up everything";
+test('returns Correct! 😄 if the punchline is correct', () => {
+  const punchline = 'because they make up everything';
   expect(tellJoke(punchline)).toBe('Correct! 😄');
+});
+
+test('returns the correct answer if punchline is incorrect', () => {
+  const incorrectPuncline = 'This is not the correct punchline';
+  const answer = 'because they make up everything';
+  expect(tellJoke(incorrectPuncline)).toBe('Nope, the correct answer is: ${answer}.');
 });
