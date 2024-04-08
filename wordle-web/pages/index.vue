5c66e880c65ce971f217017ee548eec85e9299f6
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-04-07 18:18:43 -0700

    color changes

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <v-card>
-      <v-card-title>HOME</v-card-title>
+      <v-card-title>WELCOME TO OUR SIMPLE GAME !</v-card-title>
   </v-card>
 
   <v-card class="mx-auto my-8" elevation="16" max-width="344">
       <v-card-item>
-        <v-card-title> GameSite </v-card-title>
-        <v-card-subtitle>
-          This is a placeholder
+        <v-card-title color="purple"> GAME </v-card-title>
+        <v-card-subtitle color="green">
+          This is Hadi and Cynthia's game... so far
+          Click the 'GO TO GAME' to go to the game pls
         </v-card-subtitle>
       </v-card-item>
 
