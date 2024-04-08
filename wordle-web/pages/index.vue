937a336a790513cc650e4c0b2df05772919a8a63
SaaWest <swest6@ewu.edu>
2024-04-07 21:54:44 -0700

    added some vuetify elements

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card>
     <v-card-item>
-      <v-card-title> Welcome to this beautiful website! </v-card-title>
+      <v-card-title class="text-center" > Welcome to this beautiful website! </v-card-title>
       <v-card-subtitle>
-        Click the game button... if you dare...
+        Click the game button... <strong class="text-amber-darken-4"> if you dare...</strong>
       </v-card-subtitle>
     </v-card-item>
     <v-card-text>
