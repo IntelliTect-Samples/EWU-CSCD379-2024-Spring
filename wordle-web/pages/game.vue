185c65ba3d315c690625cae3443a70950a51649f
Hadi <amallah@ewu.edu>
2024-04-07 18:50:30 -0700

    TEHEE

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card>
-    <v-card-title>Hello Welcome to Home</v-card-title>
+    <v-card-title>Hello Welcome to Hillbilly Wordle</v-card-title>
   </v-card>
 
   <v-card class="mx-auto my-8" elevation="16" max-width="344">
     <v-card-item>
       <v-card-title> Website </v-card-title>
-      <v-card-subtitle> This is our super basic website </v-card-subtitle>
+      <v-card-subtitle> This is our super basic game </v-card-subtitle>
     </v-card-item>
 
     <v-card-text>
