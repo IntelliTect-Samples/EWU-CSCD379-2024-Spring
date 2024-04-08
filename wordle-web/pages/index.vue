a940980bbdd29cb9183bb14eeab83ac34286eb3f
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-04-07 22:28:50 -0700

    updated v-card-actions margins in index.vue

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -11,7 +11,7 @@
       We made our own "Wordle" game variation called "Pink Wordle" and invite
       you to play! Have fun! :)
     </v-card-subtitle>
-    <v-card-actions class="justify-center ma-8">
+    <v-card-actions class="justify-center mx-2 my-4">
       <v-btn color="secondary" class="bg-primary" to="/game"
         >Play Pink Wordle!</v-btn
       >
