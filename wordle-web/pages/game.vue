5436f90fe4974fecc360ca14dc72d3a394b765fd
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-04-07 20:04:29 -0700

    Hill billy guessing game working

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,40 +1,54 @@
 <template>
   <v-card>
-    <v-card-title>Hillbilly Wordle</v-card-title>
+    <v-card-title>Hillbilly Guess Game</v-card-title>
   </v-card>
 
   <v-card class="mx-auto my-8" elevation="16" max-width="344">
     <v-card-item>
-      <v-card-title> Website </v-card-title>
+      <v-card-title> Hillbilly Guess of the Day </v-card-title>
       <v-card-subtitle> This is our super basic game </v-card-subtitle>
     </v-card-item>
 
     <v-card-text>
       <br />
-      My Guess:"meow"
-      <v-text-field label="Enter ANYTHING" outlined clearable />
+      WORD TO GUESS IS ....
+      <br />
+      <br />
+      _ill bill_
+      <br />
+      <br />
     </v-card-text>
+
+    <v-text-field v-model="myGuess" label="Enter your guess: " outlined clearable />
 
     <v-card-actions>
       <v-spacer />
-      <v-btn color="pink" variant="elevated" elevation="8" @click="">
-        Click Me!
+      <v-btn color="pink" variant="elevated" elevation="8" @click="checkInput()">
+        Check my answer!
       </v-btn>
-      <v-btn color="pink" variant="elevated" elevation="8" @click="router.push('/')">Home</v-btn>
+      <v-btn color="pink" variant="elevated" elevation="8" @click="router.push('/')">take me home!</v-btn>
     </v-card-actions>
+    <v-card-text>
+      YOUR GUESS: {{ myGuess }} <br />
+      WAS IT RIGHT? {{ output }}
+    </v-card-text>
     
   </v-card>
 </template>
 
 <script setup lang="ts">
-import { Game } from "@/scripts/game";
 
-const game: Game = reactive(new Game("JUMBO"));
-const myGuess = ref("");
+ var myGuess = ref("");
 
-function submitGuess() {
-  game.guess(myGuess.value.toUpperCase());
-  myGuess.value = "";
+const output = ref("");
+
+function checkInput() {
+  if(myGuess.value == "hill billy"){
+    output.value = "CORRECT"
+  }
+  else{
+    output.value = "WRONG"
+  }
 }
 const router = useRouter()
 </script>
--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,20 +1,20 @@
 <template>
   <v-card>
-      <v-card-title class="text-blue">WELCOME TO OUR SIMPLE GAME Hillbilly Wordle!</v-card-title>
+      <v-card-title class="text-blue">WELCOME TO OUR SIMPLE GAME!</v-card-title>
   </v-card>
 
   <v-card class="mx-auto my-8" elevation="16" max-width="344">
       <v-card-item>
-        <v-card-title class="text-blue"> GAME </v-card-title>
+        <v-card-title class="text-blue">  Hillbilly Wordle! </v-card-title>
       </v-card-item>
 
       <v-card-text class="text-black">
-        Hello <br/>
+        Hi guys<br/>
         <br/>
         This is Hadi and Cynthia's simple game.
         <br/>
         <br/>
-        Click the 'GO TO GAME' to go to the game!
+        Click the 'GO TO GAME' button to go to the game!
       </v-card-text>
 
       <v-card-actions>
