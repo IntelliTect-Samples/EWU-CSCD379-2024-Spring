1c4d8e63c0713a7e67fb3e0cba7c48e7d761fb59
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-04-07 17:04:37 -0700

    changes

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -5,52 +5,17 @@
 
     <v-card class="mx-auto my-8" elevation="16" max-width="344">
         <v-card-item>
-          <v-card-title> Wordle </v-card-title>
+          <v-card-title> GameSite </v-card-title>
           <v-card-subtitle>
-            This is our super basic wordle game
+            This is a placeholder
           </v-card-subtitle>
         </v-card-item>
 
         <v-card-text>
-          Hint: {{ game.wordToGuess }}
-          <br />
-          My Guess: {{ myGuess }}
-          <v-text-field
-            v-model="myGuess"
-            label="Enter your guess"
-            outlined
-            clearable
-          />
-        </v-card-text>
-
-        <v-card-text>
-          <div v-for="(guess, i) of game.guesses" :key="i">
-            Guess: {{ guess.letters.map(x => x.char).join("") }}
-          </div>
         </v-card-text>
 
         <v-card-actions>
           <v-spacer />
-          <v-btn
-            color="pink"
-            variant="elevated"
-            elevation="8"
-            @click="submitGuess()"
-          >
-            Click Me!
-          </v-btn>
         </v-card-actions>
       </v-card>
 </template>
-
-<script setup lang="ts">
-import { Game } from "../scripts/game";
-const game: Game = reactive(new Game("JUMBO"));
-
-const myGuess = ref("");
-
-function submitGuess() {
-  game.guess(myGuess.value.toUpperCase());
-  myGuess.value = "";
-}
-</script>
