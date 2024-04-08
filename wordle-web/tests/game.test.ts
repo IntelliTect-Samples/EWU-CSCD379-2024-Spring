038a65dc650100f5a74768c36befd39ba70fea92
conner hanson <connerhanson0826@gmail.com>
2024-04-07 21:50:50 -0700

    fixed joke script path

--- wordle-web/tests/game.test.ts
+++ wordle-web/tests/game.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { tellJoke } from "~/Assignment1_CSCD379/EWU-CSCD379-2024-Spring/wordle-web/scripts/joke.ts";
+import { tellJoke } from "~/scripts/joke.ts";
 
 test('returns Correct! 😄 if the punchline is correct', () => {
   const punchline = 'because they make up everything';
