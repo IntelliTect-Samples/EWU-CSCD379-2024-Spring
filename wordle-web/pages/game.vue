6cd82174eaa93bff366cb5b25bcbc019998167f3
Hadi <amallah@ewu.edu>
2024-04-07 18:11:17 -0700

    Changes

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,21 +1,25 @@
 <template>
-    <v-card>
-        <v-card-title>HOME</v-card-title>
-    </v-card>
+  <v-card>
+    <v-card-title>Hello Welcome to Home</v-card-title>
+  </v-card>
 
-    <v-card class="mx-auto my-8" elevation="16" max-width="344">
-        <v-card-item>
-          <v-card-title> GameSite </v-card-title>
-          <v-card-subtitle>
-            This is a placeholder
-          </v-card-subtitle>
-        </v-card-item>
+  <v-card class="mx-auto my-8" elevation="16" max-width="344">
+    <v-card-item>
+      <v-card-title> Website </v-card-title>
+      <v-card-subtitle> This is our super basic website </v-card-subtitle>
+    </v-card-item>
 
-        <v-card-text>
-        </v-card-text>
+    <v-card-text>
+      <br />
+      My Guess: "meow"
+      <v-text-field label="Enter ANYTHING" outlined clearable />
+    </v-card-text>
 
-        <v-card-actions>
-          <v-spacer />
-        </v-card-actions>
-      </v-card>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn color="pink" variant="elevated" elevation="8" @click="">
+        Click Me!
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
