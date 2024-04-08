fa4689870ac847c507f52ed429e29c00226ddf84
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-04-07 18:34:11 -0700

    colors

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,14 +1,12 @@
 <template>
   <v-card>
-      <v-card-title>WELCOME TO OUR SIMPLE GAME !</v-card-title>
+      <v-card-title class="pink--text accent-3">WELCOME TO OUR SIMPLE GAME !</v-card-title>
   </v-card>
 
   <v-card class="mx-auto my-8" elevation="16" max-width="344">
       <v-card-item>
-        <v-card-title color= colors.pink.accent3 variant="elevated"
-        elevation="8"> GAME </v-card-title>
-        <v-card-subtitle color="green"  variant="elevated"
-        elevation="8">
+        <v-card-title class="pink--text accent-3"> GAME </v-card-title>
+        <v-card-subtitle class="green--text">
           This is Hadi and Cynthia's game... so far Click the 'GO TO GAME' to go to the game pls
         </v-card-subtitle>
       </v-card-item>
