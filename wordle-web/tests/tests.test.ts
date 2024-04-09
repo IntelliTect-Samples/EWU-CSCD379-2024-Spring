f6d28226cd542d8653bcaa355b77a2f89ee2bceb
Hadi <amallah@ewu.edu>
2024-04-09 13:42:47 -0700

    TEST RENAME

--- wordle-web/tests/tests.test.ts
+++
@@ -1,4 +0,0 @@
-import { expect, test } from "vitest";
-test('mathtest', () => {
-    expect(1+2).toBe(3)
-  });
---
+++ wordle-web/tests/tests.ts
@@ -0,0 +1,4 @@
+import { expect, test } from "vitest";
+test('mathtest', () => {
+    expect(1+2).toBe(3)
+  });
