b202c38051aef92885a4616c40d959cba6461499
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-04-09 11:47:04 -0700

    Changing look for index

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,24 +1,23 @@
 <template>
-  <v-card>
-      <v-card-title class="text-blue">WELCOME TO OUR SIMPLE GAME!</v-card-title>
+  <v-card color="yellow" elevation="16" max-width="400">
+      <v-card-title class="text-green" >Welcome To Our Hillbilly Simple Game!</v-card-title>
   </v-card>
 
-  <v-card class="mx-auto my-8" elevation="16" max-width="344">
+  <v-card color="yellow" class="mx-auto my-10" elevation="16" max-width="344">
       <v-card-item>
-        <v-card-title class="text-blue">  Hillbilly Wordle! </v-card-title>
+        <v-card-title class="text-green">  Hillbilly Guess Game! </v-card-title>
       </v-card-item>
 
-      <v-card-text class="text-black">
+      <v-card-text class="text-green">
         Hi guys<br/>
         <br/>
         This is Hadi and Cynthia's simple game.
-        <br/>
         <br/>
         Click the 'GO TO GAME' button to go to the game!
       </v-card-text>
 
       <v-card-actions>
-      <v-btn color="blue" @click="router.push('/game')">Go To Game</v-btn>
+      <v-btn color="green" elevation="16" max-width="344" @click="router.push('/game')">Go To Game</v-btn>
       </v-card-actions>
     </v-card>
 </template>
