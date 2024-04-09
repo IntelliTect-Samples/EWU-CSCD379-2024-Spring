339991bfd616aa7d92365cfabe74e3140eff6de4
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-04-09 15:50:35 -0700

    building

--- wordle-web/tests/tests.ts
+++ wordle-web/tests/tests.ts
@@ -1,6 +1,11 @@
-import { expect, test } from "vitest";
+/*import { expect, test } from "vitest";
 import checkInput from "..\\pages\\game.vue";
 
 test('input',() =>{
   expect(new checkInput('hill billy')).contain('CORRECT');
-});    
+});  */
+
+import { expect, test } from "vitest";
+test('mathtest', () => {
+    expect(1+2).toBe(3)
+  });
