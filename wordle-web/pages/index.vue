d00c9f475f3bd11a05cf0cf424b486d9a9d18443
Meghan <Meg.Woodford@IntelliTect.com>
2024-04-11 15:28:54 -0700

    Adding on keyboard events

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -9,61 +9,31 @@
         :guess="guess"
       />
     </v-card>
-
-    <!-- Old Stuff -->
-    <div>
-      <v-divider class="my-5" />
-      <h3>My Old UI</h3>
-      <v-card>
-        <v-card-title>Hello Welcome to Home</v-card-title>
-      </v-card>
-
-      <v-card elevation="16">
-        <v-card-item>
-          <v-card-title> Wordle </v-card-title>
-          <v-card-subtitle>
-            This is our super basic wordle game
-          </v-card-subtitle>
-        </v-card-item>
-
-        <v-card-text>
-          Hint: {{ game.wordToGuess }}
-          <br />
-          My Guess: {{ myGuess }}
-          <v-text-field
-            v-model="myGuess"
-            label="Enter your guess"
-            outlined
-            clearable
-          />
-        </v-card-text>
-
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="pink"
-            variant="elevated"
-            elevation="8"
-            @click="submitGuess()"
-          >
-            Click Me!
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { Letter, LetterState } from "~/scripts/letter";
 import { Game } from "../scripts/game";
 const game: Game = reactive(new Game("JUMBO"));
 
 const myGuess = ref("");
 
-function submitGuess() {
-  game.guess(myGuess.value.toUpperCase());
-  myGuess.value = "";
+onMounted(() => {
+  window.addEventListener("keyup", onKeyup);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keyup", onKeyup);
+});
+
+function onKeyup(event: KeyboardEvent) {
+  if(event.key === "Enter") {
+    game.submitGuess();
+  } else if(event.key == 'Backspace'){
+    game.removeLastLetter();
+  } else if(event.key.match(/[A-z]/)){
+    game.addLetter(event.key.toUpperCase());
+  }
 }
 </script>
 
