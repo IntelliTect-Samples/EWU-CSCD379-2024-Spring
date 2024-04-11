c99fa4d257aaf23d0bf05a86dd6e1a06ca045b62
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-04-10 22:09:56 -0700

    added back in pink wordle game description

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -11,6 +11,15 @@
       <v-card-subtitle>
         This is our super basic Pink Wordle game! Good luck!
       </v-card-subtitle>
+      <v-card-text class="font-italic">
+        "Pink Wordle is a delightful twist on the classic word puzzle game,
+        Wordle. In this charming variant, the game's interface is adorned with
+        various shades of pink, offering a refreshing and visually pleasing
+        experience to the players. The objective remains the same - guess the
+        hidden word within six attempts.Pink Wordle combines the mental
+        challenge of the original game with a unique aesthetic, making it a
+        must-try for puzzle enthusiasts and pink lovers alike."
+      </v-card-text>
     </v-card-item>
     <v-spacer />
 
