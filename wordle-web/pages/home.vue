c21d93fac5b63aceb4dfee81cbfa07baed51568e
Bryan <bsnyder13@ewu.edu>
2024-04-11 16:34:24 -0700

    Update and rename test.vue to home.vue

---
+++ wordle-web/pages/home.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container>
+    <v-card>
+      <v-card-title>Wordle</v-card-title>
+
+      <div v-if="game.gameState == GameState.Won">
+        You've Won!
+      </div>
+      <div v-if="game.gameState == GameState.Lost">
+        You've Lost!
+      </div>
+
+      <GameBoardGuess
+        v-for="(guess, i) of game.guesses"
+        :key="i"
+        :guess="guess"
+      />
+
+      <v-card-actions>
+        <v-spacer />
+        <v-btn v-if="game.gameState !== GameState.Playing" variant="tonal" color="red" @click="game.startNewGame()">
+          Restart Game
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { Game, GameState } from "../scripts/game";
+const game: Game = reactive(new Game());
+
+const myGuess = ref("");
+
+onMounted(() => {
+  window.addEventListener("keyup", onKeyup);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keyup", onKeyup);
+});
+
+function onKeyup(event: KeyboardEvent) {
+  if(event.key === "Enter") {
+    game.submitGuess();
+  } else if(event.key == 'Backspace'){
+    game.removeLastLetter();
+  } else if(event.key.match(/[A-z]/)){
+    game.addLetter(event.key.toUpperCase());
+  }
+}
+</script>
