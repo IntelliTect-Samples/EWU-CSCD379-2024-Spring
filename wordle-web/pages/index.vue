a5ccd2b42fa8f2102ed3ed7f4c2f06c036ea04da
Bryan <bsnyder13@ewu.edu>
2024-04-11 16:48:44 -0700

    Changed home.vue back to index.vue, changed title and description

---
+++ wordle-web/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-card>
+        <v-card-title>Hello Welcome to <strong class="text-red-accent-4">LexiQuest</strong> </v-card-title>
+          <v-card-subtitle>
+          Embark on a thrilling journey through puzzles and wordplay in LexiQuest, where language meets adventure!
+          </v-card-subtitle>
+      <v-card-text>
+        <NuxtLink to="/game"> Game </NuxtLink>
+      </v-card-text>
+    </v-card>
+</template>
