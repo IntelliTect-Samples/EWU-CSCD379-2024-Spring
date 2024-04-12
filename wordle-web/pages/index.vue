1f33f476cd9055e8af421e3dea36dc59e95433c1
Bryan <bsnyder13@ewu.edu>
2024-04-11 17:24:10 -0700

    Update index.vue
    Changed description with a better hint for game

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -2,7 +2,7 @@
     <v-card>
         <v-card-title>Hello Welcome to <strong class="text-red-accent-4">LexiQuest</strong> </v-card-title>
           <v-card-subtitle>
-          Embark on a thrilling journey through puzzles and wordplay in LexiQuest, where language meets adventure!
+          Do you have the skills and knowledge to face the monsters that lie ahead? Remember when in doubt FIREBALL!
           </v-card-subtitle>
       <v-card-text>
         <NuxtLink to="/game"> Begin Quest! </NuxtLink>
