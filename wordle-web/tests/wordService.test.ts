9cc29cd6df926e5e73dcd33d45549638e066eb71
connerhanson <connerhanson0826@gmail.com>
2024-04-15 14:56:33 -0700

    created test for wordService

---
+++ wordle-web/tests/wordService.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+import { Game } from "~/scripts/game";
+import {WordList } from "~/scripts/wordList";
+import { LetterState } from "~/scripts/letter";
+import { WordService } from "~/scripts/wordService";
+
+test('WordService test full word', ()=> {
+  const guess = 'aargh';
+  const states = [
+    LetterState.Correct,
+    LetterState.Correct,
+    LetterState.Correct,
+    LetterState.Correct,
+    LetterState.Correct,
+  ];
+
+  const validWords = WordService.validGuessedWords(guess, states);
+  expect(validWords).toEqual(['aargh']);
+});
