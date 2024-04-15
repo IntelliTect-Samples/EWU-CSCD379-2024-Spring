86305ad62c9585740bb15490b8574dbd2863faf3
connerhanson <connerhanson0826@gmail.com>
2024-04-15 14:45:46 -0700

    created logic to show available words

---
+++ wordle-web/scripts/wordService.ts
@@ -0,0 +1,29 @@
+import { LetterState } from "./letter";
+import { WordList } from "./wordList";
+
+export class wordService {
+  static validGuessedWords(currentGuess: string, states: LetterState[]): string[] {
+    return WordList.filter(word => {
+      for(let i = 0; i < word.length; i++) {
+        const guessedChar = currentGuess[i].toLowerCase();
+        const isCorrectPosition = word[i] === guessedChar;
+        const isInWord = word.includes(guessedChar);
+
+        switch (states[i]) {
+          case LetterState.Correct:
+            if(!isCorrectPosition) return false;
+            break;
+          case LetterState.Misplaced:
+            if(isCorrectPosition || !isInWord) return false;
+            break;
+          case LetterState.Wrong:
+            if(isInWord) return false;
+            break;
+          case LetterState.Unknown:
+            break;
+        }
+      }
+      return true;
+    });
+  }
+}
