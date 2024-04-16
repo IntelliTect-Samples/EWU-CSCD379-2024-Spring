6b369eb9fc0307ad9c164a2e3455d381c251c18b
SaaWest <swest6@ewu.edu>
2024-04-15 23:50:12 -0700

    made changes based on feedback to logic of loop and component still needs work

---
+++ wordle-web/components/ValidWord.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-card>
+        <v-card-title>Valid Words</v-card-title>
+        <v-list>
+            <v-list-item v-for="(word, i) in WordList.validateWord()" :key="i"> 
+                    <v-list-item-title>{{ word }}</v-list-item-title>
+                </v-list-item>
+        </v-list>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import { WordList } from '~/scripts/wordList';
+</script>
