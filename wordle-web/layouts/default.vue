0227108b44f4324537ff3702c88ca132ad66de73
Joshua Lester <jlester3@ewu.edu>
2024-04-16 10:09:39 -0700

    layout-Auto stash before merge of "Assignment2" and "origin/Assignment2"

---
+++ wordle-web/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <v-app>
+    <v-btn to="/">Home</v-btn>
+    <v-btn to="/test">Test</v-btn>
+    <div>
+      <slot />
+    </div>
+  </v-app> -->
+  <v-app>
+    <v-app-bar>
+      <v-app-bar-title>Wordle Hurdle</v-app-bar-title>
+      <v-btn to="/">Home</v-btn>
+      <v-btn to="/test">Test</v-btn>
+    </v-app-bar>
+    <v-main>
+      <slot />
+    </v-main>
+  </v-app>
+</template>
