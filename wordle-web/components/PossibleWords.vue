1ca7de17e44d65b53f55632d540b541e55dff972
connerhanson <connerhanson0826@gmail.com>
2024-04-16 16:23:50 -0700

    made dropdown menu for possible words, not yet linked with words or method, added component to github

---
+++ wordle-web/components/PossibleWords.vue
@@ -0,0 +1,20 @@
+import { WordService } from "../scripts/wordService.ts";
+
+<template>
+  <div>
+    <v-select
+      clearable
+      label="Possible Words: "
+      :items="['Option1', 'Option2', 'Option3']"
+      variant="solo-filled"
+    ></v-select>
+  </div>
+</template>
+
+
+<script>
+</script>
+
+
+<style>
+</style>
