f145d8876edaf026f04d7124703ec8e3c65d4e03
Alexa <adarrington@ewu.edu>
2024-04-16 21:40:29 -0700

    Made text font bigger in about.vue

--- wordle-web/pages/about.vue
+++ wordle-web/pages/about.vue
@@ -6,15 +6,15 @@
         elevation="4"
       >
         <v-card-text class="text-center">
-          <v-card-title class="font-weight-bold" style="color: #FFFFFF;">
+          <v-card-title class="font-weight-bold" style="color: #FFFFFF;font-size:24px;">
             About <v-icon>mdi-information-outline</v-icon>
           </v-card-title>
-          <v-card-text class="font-italic" style="color: #FFFFFF;">
+          <v-card-text class="font-italic" style="color: #FFFFFF; font-size: 20px;">
             This class project is about exploring more of Vue and Vuetify 
             features and styling our wordle website more! We are adding more pages 
             and creating more features for users on the website, while also creating more features with 
             our game, like word lists and keyboard on the UI. Meg is amazing and someone I look 
-            up to, as a women in CS! She does a great job explaing things in class, and I don't 
+            up to as a women in CS! She does a great job explaing things in class, and I don't 
             know what I would do without her! #MEGFORPRESIDENT
           </v-card-text>
         </v-card-text>
