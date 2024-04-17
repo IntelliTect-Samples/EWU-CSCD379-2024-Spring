21fe798c0842a92ba099bb43a9d4436b0760100a
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-04-17 13:50:58 -0700

    adding App Bar
    Convert the sidebar to an App Bar ❌✅
    Name of the app on the left with an icon ❌✅
    Choose an icon for the app (from Material Design or somewhere on the web) ❌✅

--- wordle-web/app.vue
+++ wordle-web/app.vue
@@ -1,15 +1,40 @@
 <template>
   <NuxtLayout>
     <v-app>
-      <v-btn @click="router.push('/')">Home</v-btn>
-      <v-btn @click="router.push('/test')">Test</v-btn>
+      <v-app-bar :elevation="2">
+        <template v-slot:prepend>
+          <v-icon>mdi-wrap-disabled</v-icon>
+        </template>
+
+        <v-app-bar-title>Wordle Web</v-app-bar-title>
+
+        <v-btn @click="router.push('/')">Home</v-btn>
+        <v-btn @click="router.push('/test')">Test</v-btn>
+        <v-btn icon="mdi-weather-night" @click="toggleTheme" />
+        <v-btn icon="mdi-help-circle" @click="showHelpDialog = true" />
+      </v-app-bar>
+      <v-main>
       <NuxtPage />
+      </v-main>
     </v-app>
   </NuxtLayout>
 </template>
 
-<script setup lang="ts">
-const router = useRouter()
 
+<script setup lang="ts">
+import { useTheme } from "vuetify";
+const router = useRouter();
+const theme = useTheme();
+
+const showHelpDialog = ref(false);
+
+function toggleTheme() {
+  if (theme.global.name.value === "light") {
+    theme.global.name.value = "dark";
+  } else {
+    theme.global.name.value = "light";
+  }
+}
 </script>
+
 
--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -51,3 +51,4 @@ function onKeyup(event: KeyboardEvent) {
 }
 </script>
 
+
