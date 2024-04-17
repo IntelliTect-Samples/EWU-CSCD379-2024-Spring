0a322c0a7bc7c5c8c6ad75d8b9cc8cc2acae9033
Timothy N <tnelson32@ewu.edu>
2024-04-17 13:08:36 -0700

    Removed wordle-web and renamed

--- wordle-web/tests/game.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from "vitest";
-import { Game } from "~/scripts/game";
-
-test("game", () => {
-	const game = new Game("autos");
-	expect(game.wordToGuess.length).toBe(5);
-});
