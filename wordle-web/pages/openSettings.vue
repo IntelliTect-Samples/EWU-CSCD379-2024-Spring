9db4e181cd761e847a6cc590dcf2c4a3cd4d2b10
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-04-18 14:09:59 -0700

    WIP

---
+++ wordle-web/pages/openSettings.vue
@@ -0,0 +1,19 @@
+<template><v-dialog max-width="500">
+  <template v-slot:default="{ isActive }">
+    <v-card title="Dialog">
+      <v-card-text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text="Close Dialog"
+          @click="isActive.value = false"
+        ></v-btn>
+      </v-card-actions>
+    </v-card>
+  </template>
+</v-dialog>
+</template>
