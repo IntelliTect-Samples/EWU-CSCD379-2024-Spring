e59e2734ad93e3a60c1448b9c3ca3be13a3aad5e
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-04-18 14:00:43 -0700

    Feature: task #2 add Name of the app and icons

--- Assignment2/app.vue
+++ Assignment2/app.vue
@@ -1,7 +1,27 @@
 <template>
-    <NuxtLayout>
+  <NuxtLayout>
     <v-app>
-      <NuxtPage />
+      <v-app-bar color="primary" :elevation="2">
+        <!-- <template v-slot:prepend >
+          <v-icon color="secondary" @click="router.push('/')"> mdi-bookshelf</v-icon>
+        </template> -->
+
+        <v-btn icon="mdi-bookshelf" @click="router.push('/')"></v-btn>
+
+        <v-app-bar-title>
+          <v-btn @click="router.push('/')">Wordle</v-btn>
+        </v-app-bar-title>
+
+        <v-btn icon="mdi-menu"></v-btn>
+
+      </v-app-bar>
+      <v-main>
+        <NuxtPage />
+      </v-main>
     </v-app>
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const router = useRouter();
+</script>
