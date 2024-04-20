97a3bfbd3e7b4572a83fadd94121e126d77ab3b0
madams41 <madams41@ewu.edu>
2024-04-20 00:38:25 -0700

    centered guess button

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,40 +1,30 @@
 <template>
   <v-container>
-    <v-card>
-      <v-card class="text-center">
-        <v-alert v-if="game.gameState != GameState.Playing"
-          :color="game.gameState == GameState.Won ? 'sucess' : 'error'" tile>
-          You {{ game.gameState == GameState.Won ? 'Won' : 'Lost' }}
-          <v-card-text>
-            The word was: {{ game.secretWord }}
-          </v-card-text>
-          <br />
-          <v-btn variant="outlined" @click="game.startNewGame()">
+    <v-card class="text-center">
+      <v-alert v-if="game.gameState != GameState.Playing" :color="game.gameState == GameState.Won ? 'success' : 'error'"
+        class="mb-5" tile>
+        <h3>
+          You've
+          {{ game.gameState == GameState.Won ? "Won" : "Lost" }}
+        </h3>
+        <v-card-text>
+          The word was: <strong>{{ game.secretWord }}</strong>
+        </v-card-text>
+        <v-btn variant="outlined" @click="game.startNewGame()">
           <v-icon size="large" class="mr-2"> mdi-restart </v-icon> Restart Game
         </v-btn>
-        </v-alert>
-      </v-card>
-      <v-card-title>Wordle</v-card-title>
-
-      <div v-if="game.gameState == GameState.Won">
-        You've Won!
-      </div>
-      <div v-if="game.gameState == GameState.Lost">
-        You've Lost!
-      </div>
+      </v-alert>
+      <v-card-title v-else>Wordle</v-card-title>
 
       <GameBoardGuess v-for="(guess, i) of game.guesses" :key="i" :guess="guess" />
-      <div class="my-18">
+
+      <div class="my-10">
         <Keyboard />
       </div>
-      <v-btn color="secondary" class="justify-center" @click=game.submitGuess>Guess</v-btn>
 
-      <v-card-actions>
-        <v-spacer />
-        <v-btn v-if="game.gameState !== GameState.Playing" variant="text" color="secondary" @click="game.startNewGame()">
-          Restart Game
-        </v-btn>
-      </v-card-actions>
+      <v-btn @click="game.submitGuess()" class="mb-5" color="primary">
+        Guess!
+      </v-btn>
     </v-card>
   </v-container>
 </template>
