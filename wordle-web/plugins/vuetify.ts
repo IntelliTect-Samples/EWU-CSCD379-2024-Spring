357421260f2894a5129798e8e141ebc079770270
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-04-21 12:45:21 -0700

    added back in unknown color in themes

--- wordle-web/plugins/vuetify.ts
+++ wordle-web/plugins/vuetify.ts
@@ -1,10 +1,9 @@
 // import this after install `@mdi/font` package
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css";
 import colors from "vuetify/lib/util/colors"; // Corrected import path
 
-
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
@@ -19,9 +18,10 @@ export default defineNuxtPlugin((app) => {
             info: "#2196F3",
             success: "#4CAF50",
             warning: "#FFC107",
-            correct:"#4CAF50",
-            misplaced:"#FFC107",
-            wrong:"#BDBDBD"
+            correct: "#4CAF50",
+            misplaced: "#FFC107",
+            wrong: "#BDBDBD",
+            unknown: colors.grey.lighten3,
           },
         },
         dark: {
@@ -32,13 +32,14 @@ export default defineNuxtPlugin((app) => {
             info: "#2196F3",
             success: "#4CAF50",
             warning: "#FFC107",
-            correct:"#4CAF50",
-            misplaced:"#FFC107",
-            wrong:"#616161"
+            correct: "#4CAF50",
+            misplaced: "#FFC107",
+            wrong: "#616161",
+            unknown: colors.grey.darken3,
           },
         },
       },
     },
   });
-  app.vueApp.use(vuetify)
-})
+  app.vueApp.use(vuetify);
+});
