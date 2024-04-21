63a4e1ff4aac016b70fc724eacacbaedd20f46a2
Joshua Lester <jlester3@ewu.edu>
2024-04-21 16:14:58 -0700

    Done with validWords (?)

--- wordle-web/scripts/validWordsUtils.ts
+++ wordle-web/scripts/validWordsUtils.ts
@@ -11,7 +11,7 @@ export class ValidWordsUtils {
     this.game = game;
     this.letters = Array.from({ length: 26 }, (v, k) => {
       let character = String.fromCharCode(97 + k);
-      this.partialWord = Array.from({length: 5}, (v, k) => undefined);
+      this.partialWord = Array.from({ length: 5 }, (v, k) => undefined);
       return new LetterHelper(character);
     });
   }
@@ -29,17 +29,38 @@ export class ValidWordsUtils {
   }
 
   method(wordListWord: string): boolean {
+    let characters = wordListWord.split('');
+    for (let index = 0; index < characters.length; index++) {
+      let character = characters[index];
+      let lettersIndex = character.charCodeAt(0) - 97;
+      let letterHelper = this.letters[lettersIndex];
+      if (!letterHelper.isValidForIndex[index]) {
+        return false;
+      }
+    }
     return true;
   }
 
   setup() {
     this.game.guesses.forEach(guess => {
       let lettersArray = guess.letters;
-      for (let index = 0; index < lettersArray.length; index++)
+      for (let index = 0; index < lettersArray.length; index++) {
         if (lettersArray[index].state === LetterState.Correct) {
           this.partialWord[index] = lettersArray[index].char;
-        } else if ()
-      });
+        } else if (lettersArray[index].state === LetterState.Misplaced) {
+          let lettersIndex = lettersArray[index].char.charCodeAt(0) - 97;
+          this.letters[lettersIndex].isValidForIndex[index] = false;
+        } else if (lettersArray[index].state === LetterState.Wrong) {
+          let lettersIndex = lettersArray[index].char.charCodeAt(0) - 97;
+          this.letters[lettersIndex].isValidForIndex = [
+            false,
+            false,
+            false,
+            false,
+            false,
+          ];
+        }
+      }
     });
   }
 }
