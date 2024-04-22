bdc921c786f6cb166911586c8f73522145dc5650
Bryan Snyder <bsnyder13@ewu.edu>
2024-04-21 19:31:30 -0700

    Create game.vue
    moved index.vue to game.vue and made small changes to words

---
+++ wordle-web/pages/game.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container>
+    <v-card class="text-center">
+      <v-alert
+        v-if="game.gameState != GameState.Playing"
+        :color="game.gameState == GameState.Won ? 'success' : 'error'"
+        class="mb-5"
+        tile
+      >
+        <h3>
+          You've
+          {{ game.gameState == GameState.Won ? "You Win! 🧙‍♂️" : "Defeat... 💀" }}
+        </h3>
+        <v-card-text>
+          The word was: <strong>{{ game.secretWord }}</strong>
+        </v-card-text>
+        <v-btn variant="outlined" @click="game.startNewGame()">
+          <v-icon size="large" class="mr-2"> mdi-restart </v-icon> Restart Game
+        </v-btn>
+      </v-alert>
+      <v-card-title v-else>LexiQuest!</v-card-title>
+
+      <GameBoardGuess
+        v-for="(guess, i) of game.guesses"
+        :key="i"
+        :guess="guess"
+      />
+
+      <div class="my-10">
+        <Keyboard />
+      </div>
+
+      <v-btn @click="game.submitGuess()" class="mb-5" color="primary">
+        Guess!
+      </v-btn>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { Game, GameState } from "../scripts/game";
+const game: Game = reactive(new Game());
+
+provide("GAME", game);
+
+const myGuess = ref("");
+
+onMounted(() => {
+  window.addEventListener("keyup", onKeyup);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("keyup", onKeyup);
+});
+
+function onKeyup(event: KeyboardEvent) {
+  if (event.key === "Enter") {
+    game.submitGuess();
+  } else if (event.key == "Backspace") {
+    game.removeLastLetter();
+  } else if (event.key.match(/[A-z]/) && event.key.length === 1) {
+    game.addLetter(event.key.toUpperCase());
+  }
+}
+</script>
