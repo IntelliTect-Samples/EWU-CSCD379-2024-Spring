5099b328f804a81191d352a20d52e9c5d222affc
connerhanson <connerhanson0826@gmail.com>
2024-04-21 18:43:05 -0700

    forgot to merge with latest push from in class before starting. Had too many merge problem to do it through github so I did it manualy

--- wordle-web/components/PossibleWords.vue
+++ wordle-web/components/PossibleWords.vue
@@ -1,18 +1,45 @@
-import { WordService } from "../scripts/wordService.ts";
-
 <template>
   <div>
     <v-select
       clearable
       label="Possible Words: "
-      :items="['Option1', 'Option2', 'Option3']"
+      v-model="selectedWord";
+      :items="wordList"
       variant="solo-filled"
     ></v-select>
   </div>
 </template>
 
 
-<script>
+<script setup lang="ts">
+import { WordService } from "~/scripts/wordService";
+import { ref, onMounted } from "vue";
+import { Letter, LetterState } from "~/scripts/letter";
+import { Word } from "~/scripts/word";
+import { Game } from "~/scripts/game";
+
+const wordList = ref<string[]>([]);
+let currentGuess: Word | null = null;
+let currentGuessStates: LetterState[];
+let selectedWord = ref<string | null>(null);
+
+onMounted(()=> {
+  if(Game.guesses && Game.guesses.length > 0 && Game.guessIndex !== undefined && Game.secretWord){
+    currentGuess = Game.guesses[Game.guessIndex];
+    currentGuessStates = currentGuess.compare(Game.secretWord);
+    populateWordList();
+  }
+});
+
+const populateWordList = () => {
+  if(currentGuess && currentGuessStates){
+    const list = WordService.validGuessedWords(currentGuess, currentGuessStates);
+    console.log(list);
+    wordList.value = list;
+  }
+};
+
+
 </script>
 
 
--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,14 +1,24 @@
 <template>
   <v-container>
-    <v-card>
-      <v-card-title>Wordle</v-card-title>
-
-      <div v-if="game.gameState == GameState.Won">
-        You've Won!
-      </div>
-      <div v-if="game.gameState == GameState.Lost">
-        You've Lost!
-      </div>
+    <v-card class="text-center">
+      <v-alert
+        v-if="game.gameState != GameState.Playing"
+        :color="game.gameState == GameState.Won ? 'success' : 'error'"
+        class="mb-5"
+        tile
+      >
+        <h3>
+          You've
+          {{ game.gameState == GameState.Won ? "Won! 🥳" : "Lost... 😭" }}
+        </h3>
+        <v-card-text>
+          The word was: <strong>{{ game.secretWord }}</strong>
+        </v-card-text>
+        <v-btn variant="outlined" @click="game.startNewGame()">
+          <v-icon size="large" class="mr-2"> mdi-restart </v-icon> Restart Game
+        </v-btn>
+      </v-alert>
+      <v-card-title v-else>Wordle</v-card-title>
 
       <GameBoardGuess
         v-for="(guess, i) of game.guesses"
@@ -16,14 +26,13 @@
         :guess="guess"
       />
 
-      <v-card-actions>
-        <v-spacer />
-        <v-btn v-if="game.gameState !== GameState.Playing" variant="tonal" color="red" @click="game.startNewGame()">
-          Restart Game
-        </v-btn>
-      </v-card-actions>
-      <PossibleWords
-      />
+      <div class="my-10">
+        <Keyboard />
+      </div>
+
+      <v-btn @click="game.submitGuess()" class="mb-5" color="primary">
+        Guess!
+      </v-btn>
     </v-card>
   </v-container>
 </template>
@@ -31,6 +40,8 @@
 <script setup lang="ts">
 import { Game, GameState } from "../scripts/game";
 const game: Game = reactive(new Game());
+
+provide("GAME", game);
 
 const myGuess = ref("");
 
@@ -43,13 +54,12 @@ onUnmounted(() => {
 });
 
 function onKeyup(event: KeyboardEvent) {
-  if(event.key === "Enter") {
+  if (event.key === "Enter") {
     game.submitGuess();
-  } else if(event.key == 'Backspace'){
+  } else if (event.key == "Backspace") {
     game.removeLastLetter();
-  } else if(event.key.match(/[A-z]/)){
+  } else if (event.key.match(/[A-z]/) && event.key.length === 1) {
     game.addLetter(event.key.toUpperCase());
   }
 }
 </script>
-
