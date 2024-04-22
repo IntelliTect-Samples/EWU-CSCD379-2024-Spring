4fb7ab01f06a48b8304613a70b4c637418120f67
Hadi <amallah@ewu.edu>
2024-04-21 20:54:15 -0700

    mb

--- wordle-web/app.vue
+++ wordle-web/app.vue
@@ -3,7 +3,7 @@
   
     <v-app>
       <v-app-bar color="primary" :elevation="2">
-        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        
         <template v-slot:prepend>
           <v-icon color="secondary"> mdi-book </v-icon>
         </template>
