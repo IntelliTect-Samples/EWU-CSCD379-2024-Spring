409288b51697d86c404322d3c150ffa4a9e41b2a
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-04-21 23:23:43 -0700

    added back button

--- wordle-web/pages/about.vue
+++ wordle-web/pages/about.vue
@@ -1,25 +1,40 @@
 <template>
-    <v-container>
-      <v-card
-        color="secondary text-primary"
-        class="mx-auto mt-8 w-75 pa-8 rounded text-center"
-        elevation="4"
-      >
-        <v-card-text class="text-center">
-          <v-card-title class="font-weight-bold" style="color: #FFFFFF;font-size:24px;">
-            About
-          </v-card-title>
-          <v-card-text class="font-italic" style="color: #FFFFFF; font-size: 20px;">
-            This class project is about exploring more of Vue and Vuetify 
-            features and styling our wordle website more! We are adding more pages 
-            and creating more features for users on the website, while also creating more features with 
-            our game, like word lists and keyboard on the UI. Meg is amazing and someone I look 
-            up to as a women in CS! She does a great job explaing things in class, and I don't 
-            know what I would do without her! #MEGFORPRESIDENT
-          </v-card-text>
+  <v-container>
+    <v-card
+      color="secondary text-primary"
+      class="mx-auto mt-8 w-75 pa-8 rounded text-center"
+      elevation="4"
+    >
+      <v-card-text class="text-center">
+        <v-card-title
+          class="font-weight-bold"
+          style="color: #ffffff; font-size: 24px"
+        >
+          About
+        </v-card-title>
+        <v-card-text
+          class="font-italic"
+          style="color: #ffffff; font-size: 20px"
+        >
+          This class project is about exploring more of Vue and Vuetify features
+          and styling our wordle website more! We are adding more pages and
+          creating more features for users on the website, while also creating
+          more features with our game, like word lists and keyboard on the UI.
+          Meg is amazing and someone I look up to as a women in CS! She does a
+          great job explaing things in class, and I don't know what I would do
+          without her! #MEGFORPRESIDENT
         </v-card-text>
-        <v-spacer />
-        <!-- Additional content can go here -->
-      </v-card>
-    </v-container>
-  </template>
+      </v-card-text>
+      <v-spacer />
+      <!-- Additional content can go here -->
+      <v-card-actions class="d-flex justify-center">
+        <v-btn
+          color="white"
+          class="bg-primary pa-2 px-5"
+          to="/"
+          text="Go Home"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
