27c7d6b8fbd65fccf81f908e754e1f1aad8f6452
Nathan <nmarsee@ewu.edu>
2024-04-22 00:07:08 -0700

    findValidWords filter implementation

--- wordle-web/scripts/ValidWordList.ts
+++ wordle-web/scripts/ValidWordList.ts
@@ -1,15 +1,22 @@
-import { LetterState, type Letter } from "./letter";
+import { Letter, LetterState } from "./letter";
 import { Word } from "./word";
 import { WordList } from "./wordList";
 import { Game, } from "./game";
 
 export class ValidWordList {
-  //public static validWordList: string[] = WordList;
-
   public static findValidWords(guesses: Word[], guessIndex: number, validWordList: string[]): string[] {
-    for(const word of validWordList) {
-      
+    for(let i = 0; i < guesses[0].word.length; i++) {
+      const letter = guesses[guessIndex].letters[i];
+      if(letter.state === LetterState.Wrong) {
+        validWordList = validWordList.filter((word) => word.indexOf(letter.char) === -1);
+      }
+      if(letter.state === LetterState.Correct) {
+        validWordList = validWordList.filter((word) => word[i] === letter.char);
+      }
+      if(letter.state === LetterState.Misplaced) {
+        validWordList = validWordList.filter((word) => word[i] !== letter.char);
+      }
     }
-    return validWords;
+    return validWordList;
   }
 }
