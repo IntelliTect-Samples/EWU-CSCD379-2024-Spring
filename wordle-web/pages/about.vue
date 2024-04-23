fdf3207487cbaad37cb24a4cb946856d85004292
Alexa <adarrington@ewu.edu>
2024-04-22 19:16:13 -0700

    Fixed formatting in about using v row and v column

--- wordle-web/pages/about.vue
+++ wordle-web/pages/about.vue
@@ -14,20 +14,39 @@
         </v-card-title>
         <v-card-text
           class="font-italic"
-          style="color: #ffffff; font-size: 23px; padding-top:20px; line-height: 1.5;"
+          style="
+            color: #ffffff;
+            font-size: 23px;
+            padding: 20px 20px;
+            line-height: 1.5;
+          "
         >
-          This class project is about exploring more of Vue and Vuetify features
-          and styling our wordle website more! We are adding more pages and
-          creating more options for users on the website, while also creating
-          more features with our game, like word lists and a keyboard on the UI.
-          Meg is amazing and someone we look up to as women in CS 
-          <v-icon small class="red--text">mdi-heart</v-icon> She does a
-          great job explaining things in class, and we really appreciate her immense knowledge! 
-          <br />
-          <br />
-          <span style="font-weight: bold; background-color: #121212; color: #ffffff; padding: 2px 6px; border-radius: 4px;">
-           #MEGFORPRESIDENT
-          </span>
+          <v-row>
+            <v-col cols="12">
+              This class project is about exploring more of Vue and Vuetify
+              features and styling our wordle website more! We are adding more
+              pages and creating more options for users on the website, while
+              also creating more features with our game, like word lists and a
+              keyboard on the UI. Meg is amazing and someone we look up to as
+              women in CS
+              <v-icon small class="red--text">mdi-heart</v-icon> She does a
+              great job explaining things in class, and we really appreciate her
+              immense knowledge!
+            </v-col>
+            <v-col cols="12">
+              <span
+                style="
+                  font-weight: bold;
+                  background-color: #121212;
+                  color: #ffffff;
+                  padding: 5px 6px;
+                  border-radius: 4px;
+                "
+              >
+                #MEGFORPRESIDENT
+              </span>
+            </v-col>
+          </v-row>
         </v-card-text>
       </v-card-text>
       <v-spacer />
