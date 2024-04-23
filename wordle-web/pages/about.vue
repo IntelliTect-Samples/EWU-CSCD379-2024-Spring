051c38a0cef4c42531057465ad693c947134573a
connerhanson <connerhanson0826@gmail.com>
2024-04-22 17:16:37 -0700

    finishing changes

--- wordle-web/pages/about.vue
+++ wordle-web/pages/about.vue
@@ -3,17 +3,15 @@
     <div class="text-box">
       <p>{{ sentence }}</p>
     </div>
+    <v-btn icon="mdi-arrow-left" @click="router.push('/')"></v-btn>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      sentence: "This wordle web app is for my EWU web devlopment class, we have the greatest teacher to ever do it, her name is Meg!"
-    };
-  }
-};
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const sentence = "This wordle web app is for my EWU web devlopment class, we have the greatest teacher to ever do it, her name is Meg!";
 </script>
 
 <style scroped>
