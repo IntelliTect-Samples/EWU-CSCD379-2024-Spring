efc7fad83f7e3c32204cfaa71316cd5e2c6753a9
Alexa <adarrington@ewu.edu>
2024-04-25 12:36:05 -0700

    Tried to mock audio to pass tests

---
+++ wordle-web/tests/vitest-setup.js
@@ -0,0 +1,10 @@
+global.Audio = class {
+    constructor(src) {
+      this.src = src;
+    }
+    play() {}
+    pause() {}
+    addEventListener() {}
+    removeEventListener() {}
+    // Add other methods and properties you use
+  };
--- wordle-web/vitest.config.ts
+++ wordle-web/vitest.config.ts
@@ -1,5 +1,9 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
+  test: {
+    globals: true,
+    setupFiles: ['./tests/vitest-setup.js'], // Adjust the path as necessary
+  },
   // any custom Vitest config you require
 })
