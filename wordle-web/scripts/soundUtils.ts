04ef75f9e96c8f176f4403b520f1e7be6608a5a5
Alexa <adarrington@ewu.edu>
2024-04-25 12:22:26 -0700

    trying to fix audio not defined error

--- wordle-web/scripts/soundUtils.ts
+++ wordle-web/scripts/soundUtils.ts
@@ -1,27 +1,35 @@
 class SoundUtils {
     playClickSound(): void {
+    if (process.client) {
       const audio = new Audio('/click.mp3');
       audio.volume = 0.9;
       audio.play();
+        }
     }
     
     playWinSound(): void {
+    if (process.client) {
       const audio = new Audio('/won.mp3');
       audio.volume = 0.9;
       audio.play();
+        }
     }
   
     playLoseSound(): void {
+    if (process.client) {
       const audio = new Audio('/lose.mp3');
       audio.volume = 0.9;
       audio.play();
     }
+}
   
     playEnterSound(): void {
+        if (process.client) {
       const audio = new Audio('/success.mp3');
       audio.volume = 0.9;
       audio.play();
     }
+}
   }
   
   export default SoundUtils;
