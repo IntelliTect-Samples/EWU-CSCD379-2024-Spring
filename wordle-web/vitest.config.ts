a9eaf347d64ca5728edf66e978732d8ffad8ee57
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-04-25 23:26:37 -0700

    forgot to save before commiting

--- wordle-web/vitest.config.ts
+++ wordle-web/vitest.config.ts
@@ -1,9 +1,5 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { defineVitestConfig } from "@nuxt/test-utils/config";
 
 export default defineVitestConfig({
-  test: {
-    globals: true,
-    setupFiles: ['./tests/vitest-setup.js'], // Adjust the path as necessary
-  },
   // any custom Vitest config you require
-})
+});
