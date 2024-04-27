d45c942c2174494e9aa63238be483c011e9f39b9
Joshua Lester <jlester3@ewu.edu>
2024-04-27 11:29:18 -0700

    Created Leaderboard controller + tested

---
+++ Wordle.Api/Wordle.Api.Tests/LeaderboardControllerTests.cs
@@ -0,0 +1,26 @@
+
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Wordle.Api.Tests;
+
+[TestClass]
+public class LeaderboardControllerTests
+{
+	private readonly WebApplicationFactory<Program> _factory = new();
+
+	[TestMethod]
+	public async Task GetScores_Something_ReturnsListSizeTen()
+	{
+		// Arrange
+		var client = _factory.CreateClient();
+
+		// Act
+		var response = await client.GetAsync("/leaderboard");
+		var content = await response.Content.ReadFromJsonAsync<List<string>>();
+
+		// Assert
+		Assert.IsNotNull(content);
+		Assert.AreEqual(10, content.Count);
+	}
+}
---
+++ Wordle.Api/Wordle.Api/Controllers/LeaderboardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Wordle.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LeaderboardController : ControllerBase
+{
+	[HttpGet(Name = "GetScores")]
+	public List<string> Get()
+	{
+		return new List<string> {
+			"1",
+			"2",
+			"3",
+			"4",
+			"5",
+			"6",
+			"7",
+			"8",
+			"9",
+			"10"
+		};
+	}
+}
