5faa288f43e2c3dab208239998c5ab4b1014f342
Joshua Lester <jlester3@ewu.edu>
2024-05-02 10:32:03 -0700

    Fixed null warning on Player.cs

--- Wordle.Api/Wordle.Api/Data/Player.cs
+++ Wordle.Api/Wordle.Api/Data/Player.cs
@@ -3,7 +3,7 @@
 public class Player
 {
     public int PlayerId { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
     public int GameCount { get; set; }
     public double AverageAttempts { get; set; }
 }
--- Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("DefaultConnection not configured");
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(connectionString);
