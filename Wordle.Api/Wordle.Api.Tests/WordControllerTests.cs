6d4054779b98965ab39c1df88effb320b6271e0f
Meghan <Meg.Woodford@IntelliTect.com>
2024-05-02 15:53:29 -0700

    fix endpoint

--- Wordle.Api/Wordle.Api.Tests/WordControllerTests.cs
+++ Wordle.Api/Wordle.Api.Tests/WordControllerTests.cs
@@ -15,7 +15,7 @@ public class WordControllerTests
         var client = _factory.CreateClient();
 
         // Act
-        var response = await client.GetAsync("/word");
+        var response = await client.GetAsync("/word/randomword");
 
         // Assert
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
@@ -28,7 +28,7 @@ public class WordControllerTests
         var client = _factory.CreateClient();
 
         // Act
-        var response = await client.GetAsync("/word");
+        var response = await client.GetAsync("/word/randomword");
         var content = await response.Content.ReadAsStringAsync();
 
         // Assert
