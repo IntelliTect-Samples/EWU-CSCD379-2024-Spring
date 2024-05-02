1816195c3c0b36d7ccbcf28dce4d34afe83f3de3
Meghan <Meg.Woodford@IntelliTect.com>
2024-05-02 15:09:34 -0700

    add word of the day endpoint

--- Wordle.Api/Wordle.Api/Services/WordOfTheDayService.cs
+++ Wordle.Api/Wordle.Api/Services/WordOfTheDayService.cs
@@ -1,20 +1,51 @@
-﻿namespace Wordle.Api.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using Wordle.Api.Models;
+
+namespace Wordle.Api.Services;
 
 public class WordOfTheDayService
 {
     private readonly List<string> words = LoadWordList();
+    private static object _lock = new();
 
-    private static string? Word = null;
+    public WordleDbContext Db { get; set; }
+
+    public WordOfTheDayService(WordleDbContext db)
+    {
+        Db = db;
+    }
 
     public string GetRandomWord()
     {
-        if (Word == null)
+        Random random = new();
+        int index = random.Next(words.Count);
+        return words[index];
+    }
+
+    public async Task<string> GetWordOfTheDay(DateOnly date)
+    {
+        WordOfTheDay? wordOfTheDay = await Db.WordsOfTheDays.FirstOrDefaultAsync(words => words.Date == date);
+
+        if (wordOfTheDay is null)
         {
-            Random random = new();
-            int index = random.Next(words.Count);
-            Word = words[index];
+            lock (_lock)
+            {
+                wordOfTheDay = Db.WordsOfTheDays.FirstOrDefault(words => words.Date == date);
+                if (wordOfTheDay is null)
+                {
+                    wordOfTheDay = new()
+                    {
+                        Word = GetRandomWord(),
+                        Date = date
+                    };
+
+                    Db.WordsOfTheDays.Add(wordOfTheDay);
+                    Db.SaveChanges();
+                }
+            }
         }
-        return Word;
+
+        return wordOfTheDay.Word;
     }
 
     #region WordList
