ca9d86a805676310ed3811ebc0c899dd299b5f94
Rahat Bhatia <rbhatia@ewu.edu>
2024-05-02 22:55:12 -0700

    DBContext for Player

---
+++ Wordle.Api/Wordle.Api/Models/PlayerDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Wordle.Api.Models
+{
+    public class PlayerDBContext
+    {
+        public DbSet<Player> Players { get; set; }
+
+        public WordleDbContext(DbContextOptions<WordleDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
