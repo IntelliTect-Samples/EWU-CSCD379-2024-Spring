eb6908753a78b26f38d6bfcb752c5bb6433001b2
Rahat Bhatia <rbhatia@ewu.edu>
2024-05-02 22:40:50 -0700

    PLayer DB setup

---
+++ Wordle.Api/Wordle.Api/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wordle.Api.Models
+{
+    [Table("Player")]
+    public class Player
+    {
+        [Key]
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+        public int GameCount { get; set; }
+        public double AverageAttempts { get; set; }
+        public int AverageSecondsPerGame { get; set; } // Optional
+    }
+}
