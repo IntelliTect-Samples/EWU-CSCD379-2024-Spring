e669779d81ea906040568e53a23d5655dd26d622
Alexa <adarrington@ewu.edu>
2024-05-04 00:30:17 -0700

    Added input binding on button

--- wordle-web/components/NameDialog.vue
+++ wordle-web/components/NameDialog.vue
@@ -6,29 +6,28 @@
       </v-sheet>
       <v-card-text>
         Enter your name to start playing and save your progress!
+        <p>Current Name: {{ playerName }}</p>
       </v-card-text>
       <v-card-item>
-        <v-text-field v-model="playerName" label="Your Name"></v-text-field>
+        <v-text-field
+          v-model="playerName"
+          label="Your Name"
+          clearable
+          solo
+        ></v-text-field>
       </v-card-item>
-
       <v-divider />
-
       <v-card-actions>
         <v-spacer></v-spacer>
         <v-btn
           color="secondary"
-          variant="flat"
           text="Don't Set Name"
           @click="modelValue = false"
+          >Don't Set Name</v-btn
         >
-        </v-btn>
-        <v-btn
-          color="secondary"
-          variant="flat"
-          text="Set Name"
-          @click="modelValue = false"
+        <v-btn color="secondary" text="Set Name" @click="modelValue = false"
+          >Set Name</v-btn
         >
-        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -36,19 +35,15 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { Letter, LetterState } from "~/scripts/letter";
-import { Word } from "~/scripts/word";
-import GameBoardGuess from "~/components/GameBoardGuess.vue"; // Ensure this import if the component is used
 
 const modelValue = defineModel<boolean>({ default: false });
-
 const playerName = ref("");
 
 function closeDialog(save: boolean) {
-  if (save) {
-    console.log("Saving name:", playerName.value); // Here you could actually save the name to a store or localStorage
-    // localStorage.setItem('playerName', playerName.value); Uncomment or modify according to your context
-  }
   modelValue.value = false;
+  if (save) {
+    console.log("Saving name:", playerName.value);
+    localStorage.setItem("playerName", playerName.value);
+  }
 }
 </script>
