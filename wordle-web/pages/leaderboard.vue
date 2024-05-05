e979590f2bf19860be0e17426dd57cdea3184a5a
Alexa <adarrington@ewu.edu>
2024-05-05 16:45:44 -0700

    Animated Leaderboard title

--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -3,7 +3,7 @@
         <v-card color="secondary"
                 class="mx-auto mt-8 w-75 pa-8 rounded text-center"
                 elevation="4">
-            <v-card-title class="text-h3 mb-3"> Leaderboard </v-card-title>
+            <v-card-title class="text-h3 mb-3 animate__animated animate__tada "> Leaderboard </v-card-title>
 
             <v-card-text>
             </v-card-text>
@@ -17,3 +17,6 @@
         </v-card>
     </v-container>
 </template>
+<script setup lang="ts">
+import 'animate.css';
+</script>
