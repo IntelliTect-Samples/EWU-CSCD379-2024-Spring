fff49193ff3b60a83990489866ba4832dc1698fc
JohanneJayde <jmcclenahan@ewu.edu>
2024-05-06 12:04:26 -0700

    updated NameDialog to match ohter dialouge

--- wordle-web/components/NameDialog.vue
+++ wordle-web/components/NameDialog.vue
@@ -1,15 +1,25 @@
 <template>
-  <v-dialog v-model="showModel" max-width="350" persistent>
+  <v-dialog v-model="showModel" max-width="500" persistent>
     <v-card>
       <v-sheet color="secondary">
         <v-card-title>Enter your name to start playing!</v-card-title>
       </v-sheet>
-      <v-text-field
-        append-inner-icon="mdi-arrow-right-bold"
-        label="Name"
-        v-model="nameModel"
-        @click:append-inner="$emit('entered')"
-      ></v-text-field>
+      <v-card-item>
+        <v-text-field
+          label="Name"
+          v-model="nameModel"
+          @click:append-inner="$emit('entered')"
+        />
+      </v-card-item>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn
+          color="secondary"
+          variant="flat"
+          text="Close"
+          @click="showModel = false"
+        />
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
