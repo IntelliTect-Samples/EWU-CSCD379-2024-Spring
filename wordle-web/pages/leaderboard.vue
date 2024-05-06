53feb4e40ddb8a08a5e2fdc5f6c99ac376c33619
Alexa <adarrington@ewu.edu>
2024-05-05 17:18:35 -0700

    Added fireworks animation to leaderboard

--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -1,22 +1,28 @@
 <template>
     <v-container>
-        <v-card color="secondary"
-                class="mx-auto mt-8 w-75 pa-8 rounded text-center"
-                elevation="4">
-            <v-card-title class="text-h3 mb-3 animate__animated animate__tada "> Leaderboard </v-card-title>
-
-            <v-card-text>
-            </v-card-text>
-            <v-spacer />
-            <v-card-actions class="d-flex justify-center">
-                <v-btn color="white"
-                       class="bg-primary pa-2 px-5"
-                       to="/"
-                       text="Go Home" />
-            </v-card-actions>
-        </v-card>
+      <div class="pyro">
+        <div class="before"></div>
+        <div class="after"></div>
+      </div>
+  
+      <v-card color="secondary"
+              class="mx-auto mt-8 w-75 pa-8 rounded text-center"
+              elevation="4">
+        <v-card-title class="text-h3 mb-3 animate__animated animate__tada">Leaderboard</v-card-title>
+        <v-card-text>
+        </v-card-text>
+        <v-spacer />
+        <v-card-actions class="d-flex justify-center">
+          <v-btn color="white"
+                 class="bg-primary pa-2 px-5"
+                 to="/"
+                 text="Go Home" />
+        </v-card-actions>
+      </v-card>
     </v-container>
-</template>
-<script setup lang="ts">
+  </template>
+  
+  <script setup lang="ts">
+import '../animations/fireworks.scss';
 import 'animate.css';
-</script>
+  </script>
