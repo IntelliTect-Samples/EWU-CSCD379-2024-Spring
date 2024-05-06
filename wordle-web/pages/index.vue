65a90c56492968e8c13a46428d6a4cfe2bdf923e
Rahat Bhatia <rbhatia@ewu.edu>
2024-05-05 23:29:55 -0700

    leaderboard works

--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -1,39 +1,42 @@
 <template>
   <v-container> 
-    <v-card class="text-center">
-      <v-alert
-        v-if="game.gameState != GameState.Playing"
-        :color="game.gameState == GameState.Won ? 'success' : 'error'"
-        class="mb-5"
-        tile
-      >
-        <h3>
-          You've
-          {{ game.gameState == GameState.Won ? "Won! 🥳" : "Lost... 😭" }}
-        </h3>
-        <v-card-text>
-          The word was: <strong>{{ game.secretWord }}</strong>
-        </v-card-text>
-        <v-btn variant="outlined" @click="game.startNewGame()">
-          <v-icon size="large" class="mr-2"> mdi-restart </v-icon> Restart Game
-        </v-btn>
-      </v-alert>
-      <v-card-title v-else>Wordle</v-card-title>
+      <v-card class="text-center">
+          <v-alert v-if="game.gameState != GameState.Playing"
+                   :color="game.gameState == GameState.Won ? 'success' : 'error'"
+                   class="mb-5"
+                   tile>
+              <h3>
+                  You've
+                  {{ game.gameState == GameState.Won ? "Won! 🥳" : "Lost... 😭" }}
+              </h3>
+              <v-card-text>
+                  The word was: <strong>{{ game.secretWord }}</strong>
+              </v-card-text>
+              <v-btn variant="outlined" @click="game.startNewGame()">
+                  <v-icon size="large" class="mr-2"> mdi-restart </v-icon> Restart Game
+              </v-btn>
+          </v-alert>
+          <v-card-title v-else>Wordle</v-card-title>
 
-      <GameBoardGuess
-        v-for="(guess, i) of game.guesses"
-        :key="i"
-        :guess="guess"
-      />
+          <GameBoardGuess v-for="(guess, i) of game.guesses"
+                          :key="i"
+                          :guess="guess" />
 
-      <div class="my-10">
-        <Keyboard />
-      </div>
+          <div class="my-10">
+              <Keyboard />
+          </div>
 
-      <v-btn @click="game.submitGuess()" class="mb-5" color="primary">
-        Guess!
-      </v-btn>
-    </v-card>
+          <v-btn @click="game.submitGuess()" class="mb-5" color="primary">
+              Guess!
+          </v-btn>
+
+          <v-btn @click="
+          $router.push('/leaderboard');"
+                 class="mb-5 mx-5"
+                 color="primary">
+              Leaderboard
+          </v-btn>
+      </v-card>
   </v-container>
 </template>
 
@@ -81,19 +84,19 @@ function onKeyup(event: KeyboardEvent) {
   }
     }
 
-function postScore() {
-    let attempts = 0;
-    if (game.value.gameState == GameState.Won) {
-        attempts = game.value.guessIndex + 1;
-    } else {
-        attempts = game.value.maxAttempts;
+    function postScore() {
+        let attempts = 0;
+        if (game.value.gameState == GameState.Won) {
+            attempts = game.value.guessIndex + 1;
+        } else {
+            attempts = game.value.maxAttempts;
+        }
+        Axios.post(apiUrl + '/leaderboard/postscore', {
+            Name: nuxtStorage.localStorage.get("userName", userName.value),
+            GameCount: 1,
+            AverageAttempts: attempts,
+        });
     }
-    Axios.post(apiUrl + '/leaderboard/postscore', {
-        Name: nuxtStorage.localStorage.get(“userName”, userNameVariable),
-        GameCount: 1,
-        AverageAttempts: attempts,
-    });
-
 
 
 </script>
---
+++ wordle-web/pages/leaderboard.vue
@@ -0,0 +1,56 @@
+<template>
+    <v-card class="my-7 mx-auto w-75">
+        <v-sheet color="secondary">
+            <v-card-title>Leaderboard</v-card-title>
+        </v-sheet>
+        <v-table class="mt-7 mx-auto w-75">
+            <thead>
+                <tr>
+                    <th class="text-h6 text-center Name"><strong>Player Name</strong></th>
+                    <th class="text-h6 text-center GameCount">
+                        <strong>Game Count</strong>
+                    </th>
+                    <th class="text-center text-h6 AverageAttempts">
+                        <strong>Average Guesses</strong>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="player in topTenScores" :key="player.name">
+                    <td class="text-center">{{ player.name }}</td>
+                    <td class="text-center">{{ player.gameCount }}</td>
+                    <td class="text-center">{{ player.averageAttempts }}</td>
+                </tr>
+            </tbody>
+        </v-table>
+        <v-container class="text-center">
+            <v-btn color="secondary" class="mb-5" @click="$router.push('/')">Home</v-btn>
+        </v-container>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue';
+    import Axios from 'axios';
+
+const apiUrl = '';
+
+
+interface Player {
+  name: string;
+  gameCount: number;
+  averageAttempts: number;
+}
+
+    const topTenScores = ref<Player[]>();
+
+
+Axios.get(apiUrl + '/leaderboard/GetScores')
+  .then(response => {
+    topTenScores.value = response.data;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+</script>
