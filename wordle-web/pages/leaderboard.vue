dc33cd5da4733fd2ed2ead4a8260c3a2c7b68f80
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-05-06 18:08:24 -0700

    leaderboard fixing

--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -1,26 +1,30 @@
 <template>
   <v-container class="text-center">
-    <v-card class="mx-auto" max-width="800" outlined>
+    <v-card class="mx-auto" max-width="1000" outlined>
       <v-card-title class="text-h5">LEADERBOARD</v-card-title>
       <v-card-text>
         <v-row>
-          <v-col cols="4">
+          <v-col cols="1" class="text-center">
+            ID
+          </v-col>
+          <v-col cols="6 " class="text-center">
             NAME 
           </v-col>
-          <v-col cols="4">
-            AVG GUESSES
+          <v-col cols="2" class="">
+            AVG ATTEMPTS
           </v-col>
-          <v-col cols="4">
-            PLAYS
+          <v-col cols="2" class="text-center" >
+            PLAYS 
           </v-col>
         </v-row>
         <v-divider></v-divider>
         <v-table>
           <tbody>
             <tr v-for="(player, index) in leaderboard" :key="index">
-              <td class="text-left">{{ player.name }}</td>
-              <td>{{ player.averageGuesses }}</td>
-              <td>{{ player.plays }}</td>
+              <td class="text-left">{{ player.id }}</td>
+              <td class="text-center">{{ player.name }}</td>
+              <td class="text-center">{{ player.averageGuesses }}</td>
+              <td class="text-center">{{ player.plays }}</td>
             </tr>
           </tbody>
         </v-table>
@@ -32,9 +36,9 @@
 <script setup>
 //SAMPLE DATA FOR NOW :(
 const leaderboard = ref([
-  { name: 'Player 1', averageGuesses: 5, plays: 10 },
-  { name: 'Player 2', averageGuesses: 4, plays: 15 },
-  { name: 'Player 3', averageGuesses: 6, plays: 8 },
+  {id: 1, name: 'Player 1', averageGuesses: 5, plays: 10 },
+  {id : 2, name: 'Player 2', averageGuesses: 4, plays: 15 },
+  {id : 3,name: 'Player 3', averageGuesses: 6, plays: 8 },
 
 ]);
 </script>
