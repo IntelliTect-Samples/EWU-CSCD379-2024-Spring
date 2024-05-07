a318d432aac9ddcca11ce1fc77732d4eeaa02965
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-05-07 03:10:04 -0700

    aded in rudimentary leaderboard feature

--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -1,27 +1,34 @@
 <template>
-    <v-container>
-      <div class="pyro">
-        <div class="before"></div>
-        <div class="after"></div>
-      </div>
-  
-      <v-card color="secondary"
-              class="mx-auto mt-8 w-75 pa-8 rounded text-center"
-              elevation="4">
-        <v-card-title class="text-h3 mb-3">Leaderboard</v-card-title>
-        <v-card-text>
-        </v-card-text>
-        <v-spacer />
-        <v-card-actions class="d-flex justify-center">
-          <v-btn color="white"
-                 class="bg-primary pa-2 px-5"
-                 to="/"
-                 text="Go Home" />
-        </v-card-actions>
-      </v-card>
-    </v-container>
-  </template>
-  
-  <script setup lang="ts">
-import '../animations/fireworks.scss';
-  </script>
+  <v-container>
+    <div class="pyro">
+      <div class="before"></div>
+      <div class="after"></div>
+    </div>
+
+    <v-card
+      color="secondary"
+      class="mx-auto mt-8 w-75 pa-8 rounded text-center"
+      elevation="4"
+    >
+      <v-card-title class="text-h3 mb-3">Leaderboard</v-card-title>
+      <v-data-table-server :items="players" />
+
+      <v-spacer />
+      <v-card-actions class="d-flex justify-center">
+        <v-btn
+          color="white"
+          class="bg-primary pa-2 px-5"
+          to="/"
+          text="Go Home"
+        />
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import "../animations/fireworks.scss";
+import Axios from "axios";
+
+const players = await Axios.get("player/leaderboard").then((res) => res.data);
+</script>
