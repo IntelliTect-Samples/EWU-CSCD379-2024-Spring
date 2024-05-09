c7749833eaa6241fa630aedc4c38e196a0dbc8d0
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-05-08 23:36:32 -0700

    added in migration for AverageSeconds

---
+++ Wordle.Api/Wordle.Api/Migrations/20240509063442_averageSeconds.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wordle.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class averageSeconds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AverageSeconds",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageSeconds",
+                table: "Players");
+        }
+    }
+}
