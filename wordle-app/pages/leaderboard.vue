a3f2fde4178ca2920c987f396ab0094755566cc4
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-05-09 14:09:42 -0700

    Fix: refactoring and fixing

--- wordle-app/pages/leaderboard.vue
+++ wordle-app/pages/leaderboard.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script setup lang="ts">
-import type { Player } from "../types/Player"
+import type { Player } from "../scripts/player"
 import Axios from "axios";
 
 const players = ref<Player[]>();
