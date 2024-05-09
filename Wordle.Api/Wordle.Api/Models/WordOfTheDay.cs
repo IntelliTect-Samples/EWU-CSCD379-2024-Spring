d5f0f73e8410f2180d42cfc46f28fb5923c46332
alexa darrington <adarrington@ewu.edu>
2024-05-09 11:16:19 -0700

    Removed comment

--- Wordle.Api/Wordle.Api/Models/WordOfTheDay.cs
+++ Wordle.Api/Wordle.Api/Models/WordOfTheDay.cs
@@ -8,8 +8,6 @@ namespace Wordle.Api.Models
     {
         public int WordOfTheDayId { get; set; }
         public string Word { get; set; } = null!;
-        public DateOnly Date { get; set; }
-        //comment
-        
+        public DateOnly Date { get; set; }        
     }
 }
