d80a0ce23709ddc319c1828e4eae5fdba4a61f6c
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-05-09 14:17:33 -0700

    fix: Remove post form game.ts

--- wordle-app/scripts/game.ts
+++ wordle-app/scripts/game.ts
@@ -99,11 +99,11 @@ export class Game {
 
     if (isCorrect) {
       this.gameState = GameState.Won;
-      this.postScore();
+      //this.postScore();
     } else {
       if (this.guessIndex === this.maxAttempts - 1) {
         this.gameState = GameState.Lost;
-        this.postScore();
+        //this.postScore();
       } else {
         this.guessIndex++;
       }
@@ -124,20 +124,20 @@ export class Game {
     }
   }
 
-  public postScore() {
-    let attempts = 0;
-    if (this.gameState == GameState.Won) {
-      attempts = this.guessIndex + 1;
-    } else {
-      attempts = this.maxAttempts;
-    }
-    axios.post("https://wordleapiewusergeitim.azurewebsites.net/Player/AddPlayer", {
-      Name: nameUserNameDialog,
-      GameCount: 1,
-      AverageAttempts: attempts,
-      AverageSecondsPerGame: stopwatch.seconds.value
-    });
-  }
+  // public postScore() {
+  //   let attempts = 0;
+  //   if (this.gameState == GameState.Won) {
+  //     attempts = this.guessIndex + 1;
+  //   } else {
+  //     attempts = this.maxAttempts;
+  //   }
+  //   axios.post("https://wordleapiewusergeitim.azurewebsites.net/Player/AddPlayer", {
+  //     Name: nameUserNameDialog,
+  //     GameCount: 1,
+  //     AverageAttempts: attempts,
+  //     AverageSecondsPerGame: stopwatch.seconds.value
+  //   });
+  // }
 }
 
 export enum GameState {
