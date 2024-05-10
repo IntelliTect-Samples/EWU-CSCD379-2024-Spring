cac1a31ebe1ba60e5517f4c5b403500d9d2afcfc
Hadi <amallah@ewu.edu>
2024-05-09 22:50:38 -0700

    Format

--- wordle-web/app.vue
+++ wordle-web/app.vue
@@ -1,13 +1,10 @@
 <template>
   <NuxtLayout>
-  
     <v-app>
       <v-app-bar color="primary" :elevation="2">
         <v-app-bar-title @click="router.push('/')">
           Wordle Web
-          <v-icon @click="router.push('/')">
-            mdi-lightbulb-on-outline
-          </v-icon>
+          <v-icon @click="router.push('/')"> mdi-lightbulb-on-outline </v-icon>
         </v-app-bar-title>
       </v-app-bar>
 
@@ -17,17 +14,26 @@
             <v-card-text>UserName: </v-card-text>
           </v-sheet>
           <v-form class="mx-3 mt-5">
-            <v-text-field @keyup.stop v-model="usersNameInput" label="user" variant="outlined" clearable required>
-              </v-text-field>
+            <v-text-field
+              @keyup.stop
+              v-model="usersNameInput"
+              label="user"
+              variant="outlined"
+              clearable
+              required
+            >
+            </v-text-field>
           </v-form>
           <v-card-actions class="mx-4 mb-3">
             <v-spacer></v-spacer>
-            <v-btn color="success" @click="saveUserName"> PRESS HERE TO SAVE</v-btn>
+            <v-btn color="success" @click="saveUserName">
+              PRESS HERE TO SAVE</v-btn
+            >
           </v-card-actions>
         </v-card>
       </v-dialog>
 
-    >
+      >
       <HelpDialog v-model="showHelpDialog" />
 
       <v-navigation-drawer expand-on-hover rail location="right">
@@ -46,60 +52,138 @@
 
         <v-divider></v-divider>
         <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-home" title="Home" @click="router.push('/')"/>
-          <v-list-item prepend-icon="mdi-cog-outline" title="Settings" @click="settingsDialog = true"/>
-          <v-list-item prepend-icon="mdi-help-circle" title="About" @click="router.push('/aboutPage')"/>
-          <v-list-item prepend-icon="mdi-weather-night" title="Light vs Dark"@click="toggleTheme()"/>
-          <v-list-item  prepend-icon="mdi-help" title="Help" @click="showHelpDialog = true" />
-          <v-list-item  prepend-icon="mdi-trophy" title="LEADERBOARD" @click="router.push('/leaderboard')" />
+          <v-list-item
+            prepend-icon="mdi-home"
+            title="Home"
+            @click="router.push('/')"
+          />
+          <v-list-item
+            prepend-icon="mdi-cog-outline"
+            title="Settings"
+            @click="settingsDialog = true"
+          />
+          <v-list-item
+            prepend-icon="mdi-help-circle"
+            title="About"
+            @click="router.push('/aboutPage')"
+          />
+          <v-list-item
+            prepend-icon="mdi-weather-night"
+            title="Light vs Dark"
+            @click="toggleTheme()"
+          />
+          <v-list-item
+            prepend-icon="mdi-help"
+            title="Help"
+            @click="showHelpDialog = true"
+          />
+          <v-list-item
+            prepend-icon="mdi-trophy"
+            title="LEADERBOARD"
+            @click="router.push('/leaderboard')"
+          />
         </v-list>
       </v-navigation-drawer>
-      
+
       <!-- Settings Dialog -->
       <v-dialog v-model="settingsDialog" max-width="500">
-        <v-card style="outline-style: inset;" color="primary">
+        <v-card style="outline-style: inset" color="primary">
           <v-sheet color="secondary">
-            <v-card-text style="text-align: center; font-size:1.75em; font-family:'Courier New'"> Settings </v-card-text>
+            <v-card-text
+              style="
+                text-align: center;
+                font-size: 1.75em;
+                font-family: 'Courier New';
+              "
+            >
+              Settings
+            </v-card-text>
           </v-sheet>
           <v-card-text>
             <v-container>
-              <v-btn class="buttonClass" @click="themeSettings('light')" style="outline-style: outset;" color="primary">Light Mode</v-btn>
+              <v-btn
+                class="buttonClass"
+                @click="themeSettings('light')"
+                style="outline-style: outset"
+                color="primary"
+                >Light Mode</v-btn
+              >
               &nbsp;&nbsp;&nbsp;
-              <v-btn class="buttonClass" @click="themeSettings('dark')" style="outline-style: outset;" color="primary">Dark Mode</v-btn>
+              <v-btn
+                class="buttonClass"
+                @click="themeSettings('dark')"
+                style="outline-style: outset"
+                color="primary"
+                >Dark Mode</v-btn
+              >
               &nbsp;&nbsp;&nbsp;
-              <v-btn class="buttonClass" @click="themeSettings('bCasual')" style="outline-style: outset;" color="primary">Business Casual</v-btn>
+              <v-btn
+                class="buttonClass"
+                @click="themeSettings('bCasual')"
+                style="outline-style: outset"
+                color="primary"
+                >Business Casual</v-btn
+              >
               &nbsp;&nbsp;&nbsp;
-              <v-btn class="buttonClass" @click="themeSettings('bCasualDark')" style="outline-style: outset;" color="primary">Business Casual -- DARK</v-btn>
+              <v-btn
+                class="buttonClass"
+                @click="themeSettings('bCasualDark')"
+                style="outline-style: outset"
+                color="primary"
+                >Business Casual -- DARK</v-btn
+              >
               &nbsp;&nbsp;&nbsp;
-              <v-btn class="buttonClass" @click="themeSettings('hillBilly')" style="outline-style: outset;" color="primary">Luigi</v-btn>
+              <v-btn
+                class="buttonClass"
+                @click="themeSettings('hillBilly')"
+                style="outline-style: outset"
+                color="primary"
+                >Luigi</v-btn
+              >
               &nbsp;&nbsp;&nbsp;
-              <v-btn class="buttonClass" @click="themeSettings('hillBillyDark')" style="outline-style: outset;" color="primary">Mario</v-btn>
+              <v-btn
+                class="buttonClass"
+                @click="themeSettings('hillBillyDark')"
+                style="outline-style: outset"
+                color="primary"
+                >Mario</v-btn
+              >
               &nbsp;&nbsp;&nbsp;
-              <v-btn class="buttonClass" @click="themeSettings('hadi')" style="outline-style: outset;" color="primary">Hadi's Choice</v-btn>
+              <v-btn
+                class="buttonClass"
+                @click="themeSettings('hadi')"
+                style="outline-style: outset"
+                color="primary"
+                >Hadi's Choice</v-btn
+              >
               &nbsp;&nbsp;&nbsp;
-              <v-btn class="buttonClass" @click="themeSettings('hadiDark')" style="outline-style: outset;" color="primary">Hadi's Choice -- DARK</v-btn>
+              <v-btn
+                class="buttonClass"
+                @click="themeSettings('hadiDark')"
+                style="outline-style: outset"
+                color="primary"
+                >Hadi's Choice -- DARK</v-btn
+              >
             </v-container>
           </v-card-text>
         </v-card>
       </v-dialog>
 
-    
       <v-main>
         <NuxtPage />
       </v-main>
-
     </v-app>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import { useTheme } from "vuetify";
-import nuxtStorage from 'nuxt-storage';
+import nuxtStorage from "nuxt-storage";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 
 const dialogBox = ref<boolean>(true);
-const usersNameInput = ref<string>('');
+const usersNameInput = ref<string>("");
 const router = useRouter();
 const theme = useTheme();
 const showHelpDialog = ref(false);
@@ -112,7 +196,8 @@ onMounted(() => {
 });
 
 function toggleTheme() {
-  theme.global.name.value = theme.global.name.value === "light" ? "dark" : "light";
+  theme.global.name.value =
+    theme.global.name.value === "light" ? "dark" : "light";
   nuxtStorage.localStorage.setData("theme", theme.global.name.value);
 }
 
@@ -121,23 +206,23 @@ function themeSettings(item: string) {
   nuxtStorage.localStorage.setData("themes", theme.global.name.value);
 }
 
-function showUser(){
+function showUser() {
   const userName = usersNameInput.value.trim();
 }
 
 function saveUserName() {
-  if (dialogBox.value) { 
+  if (dialogBox.value) {
     const userName = usersNameInput.value.trim();
-    if (userName !== '') {
+    if (userName !== "") {
       nuxtStorage.localStorage.setData("userName", userName);
       dialogBox.value = false;
-    } 
+    }
   }
 }
 </script>
 
 <style>
-.buttonClass{
+.buttonClass {
   display: flex;
   justify-content: center;
   border: 2px;
