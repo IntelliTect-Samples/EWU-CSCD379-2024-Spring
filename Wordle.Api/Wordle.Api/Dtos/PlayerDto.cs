1337e2c84350131131a5afdce01a50f129ec7338
Hadi <amallah@ewu.edu>
2024-05-10 00:47:15 -0700

    API

--- Wordle.Api/Wordle.Api/Dtos/PlayerDto.cs
+++ Wordle.Api/Wordle.Api/Dtos/PlayerDto.cs
@@ -2,7 +2,7 @@
 
 public record PlayerDto
 {
-	public string Name { get; set; }
+	public string? Name { get; set; }
 	public int GameCount { get; set; }
 	public double AverageAttempts { get; set; }
 }
