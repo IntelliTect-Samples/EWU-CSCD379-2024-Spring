3b1920c3611af6d8d560b402a4ee153c2dd2b252
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-05-10 18:53:02 -0700

    fixed game and wordlist tests

--- wordle-web/tests/game.test.ts
+++ wordle-web/tests/game.test.ts
@@ -1,20 +1,38 @@
-// // @vitest-environment nuxt
-// import { expect, test } from "vitest";
-// import { Game } from "~/scripts/game";
+// @vitest-environment nuxt
+import { expect, test } from "vitest";
+import { Game } from "~/scripts/game";
 
-// test("game", () => {
-//   // create game and check if it's created
-//   const game = new Game("GAMES");
-//   expect(game.secretWord.length).toBe(5);
-// });
+test("startNewGame secret word length is 5", async () => {
+  // create game and check if it's created
+  const game = new Game();
+  await game.startNewGame();
+  expect(game.secretWord.length).toBe(5);
+});
 
-// test("guess-word", () => {
-//   const game = new Game("GAMES");
-//   game.guess.addLetter("Z");
-//   game.guess.addLetter("Z");
-//   game.guess.addLetter("Z");
-//   game.guess.addLetter("Z");
-//   game.guess.addLetter("Z");
-//   game.guess.addLetter("Z");
-//   expect(game.guess.letters.length).toBe(5);
-// });
+test("submitGuess valid guess increases guess index ", async () => {
+  const game = new Game();
+  await game.startNewGame();
+
+  game.addLetter("S");
+  game.addLetter("M");
+  game.addLetter("A");
+  game.addLetter("L");
+  game.addLetter("L");
+  game.addLetter("S");
+  game.submitGuess();
+  expect(game.guessIndex).toBe(1);
+});
+
+test("submitGuess invaid guess does not increase guess index ", async () => {
+  const game = new Game();
+  await game.startNewGame();
+
+  game.addLetter("Z");
+  game.addLetter("Z");
+  game.addLetter("Z");
+  game.addLetter("Z");
+  game.addLetter("Z");
+  game.addLetter("Z");
+  game.submitGuess();
+  expect(game.guessIndex).toBe(0);
+});
--- wordle-web/tests/wordlist.test.ts
+++ wordle-web/tests/wordlist.test.ts
@@ -1,49 +1,54 @@
-// import { expect, test } from "vitest";
-// import { Game } from "~/scripts/game";
-// import { WordList } from "~/scripts/wordList";
-// import { filterValidWords } from "~/scripts/wordListUtils";
+import { expect, test } from "vitest";
+import { Game } from "~/scripts/game";
+import { WordList } from "~/scripts/wordList";
+import { filterValidWords } from "~/scripts/wordListUtils";
 
-// test("filterValidWords init contains all wordlist", () => {
-//   const game = new Game("games");
-//   // test if filterValidWords returns an array
-//   expect(filterValidWords(game).length).equal(WordList.length);
-// });
+test("filterValidWords init contains all wordlist", () => {
+  const game = new Game();
+  // test if filterValidWords returns an array
+  expect(filterValidWords(game).length).equal(WordList.length);
+});
 
-// test("filterValidWords removes words with wrong letters", () => {
-//   const game = new Game("hello");
-//   game.guess.addLetter("H");
-//   game.guess.addLetter("A");
-//   game.guess.addLetter("N");
-//   game.guess.addLetter("D");
-//   game.guess.addLetter("S");
-//   game.submitGuess();
+test("filterValidWords removes words with wrong letters", async () => {
+  const game = new Game();
+  await game.startNewGame("hello");
+  game.guess.addLetter("H");
+  game.guess.addLetter("A");
+  game.guess.addLetter("N");
+  game.guess.addLetter("D");
+  game.guess.addLetter("S");
+  game.submitGuess();
 
-//   expect(filterValidWords(game)).not.toContain("hands");
-//   expect(filterValidWords(game)).not.toContain("snare");
-// });
+  expect(filterValidWords(game)).not.toContain("hands");
+  expect(filterValidWords(game)).not.toContain("snare");
+});
 
-// test("filterValidWords removes words with correct letters in wrong position", () => {
-//   const game = new Game("hello");
-//   game.guess.addLetter("L");
-//   game.guess.addLetter("A");
-//   game.guess.addLetter("M");
-//   game.guess.addLetter("P");
-//   game.guess.addLetter("S");
-//   game.submitGuess();
+test("filterValidWords removes words with correct letters in wrong position", async () => {
+  const game = new Game();
+  await game.startNewGame("hello");
 
-//   expect(filterValidWords(game)).not.toContain("loads");
-//   expect(filterValidWords(game)).not.toContain("laced");
-// });
+  game.guess.addLetter("L");
+  game.guess.addLetter("A");
+  game.guess.addLetter("M");
+  game.guess.addLetter("P");
+  game.guess.addLetter("S");
+  game.submitGuess();
 
-// test("filterValidWords contains words with letters that are misplaced in different position", () => {
-//   const game = new Game("clear");
-//   game.guess.addLetter("p");
-//   game.guess.addLetter("u");
-//   game.guess.addLetter("l");
-//   game.guess.addLetter("s");
-//   game.guess.addLetter("e");
-//   game.submitGuess();
+  expect(filterValidWords(game)).not.toContain("loads");
+  expect(filterValidWords(game)).not.toContain("laced");
+});
 
-//   expect(filterValidWords(game)).toContain("biggy");
-//   expect(filterValidWords(game)).toContain("drink");
-// });
+test("filterValidWords contains words with letters that are misplaced in different position", async () => {
+  const game = new Game();
+  await game.startNewGame("hello");
+
+  game.guess.addLetter("p");
+  game.guess.addLetter("u");
+  game.guess.addLetter("l");
+  game.guess.addLetter("s");
+  game.guess.addLetter("e");
+  game.submitGuess();
+
+  expect(filterValidWords(game)).toContain("biggy");
+  expect(filterValidWords(game)).toContain("drink");
+});
