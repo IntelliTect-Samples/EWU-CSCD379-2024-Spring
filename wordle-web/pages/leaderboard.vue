37ef4cd0f2aeaf9781932363f85946b7605c1789
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-12 22:42:58 -0700

    Update leaderboard.vue

--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -1,17 +1,17 @@
 <template>
-  <v-card class="my-7 mx-auto w-75" elevation="10" shaped>
-    <v-sheet color="deep-purple accent-4">
-      <v-card-title class="display-1 white--text">LexiQuest Leaderboard</v-card-title>
+  <v-card class="my-7 mx-auto w-75">
+    <v-sheet color="secondary">
+      <v-card-title>Leaderboard</v-card-title>
     </v-sheet>
-    <v-table class="elevation-5 mt-7 mx-auto w-75">
+    <v-table class="table mt-7 mx-auto w-75">
       <thead>
         <tr>
-          <th class="text-h6 text-center Name"><strong>Adventurer</strong></th>
+          <th class="text-h6 text-center Name"><strong>Player Name</strong></th>
           <th class="text-h6 text-center GameCount">
-            <strong>Quests Completed</strong>
+            <strong>Game Count</strong>
           </th>
           <th class="text-center text-h6 AverageAttempts">
-            <strong>Average Attempts</strong>
+            <strong>Average Guesses</strong>
           </th>
         </tr>
       </thead>
@@ -24,9 +24,9 @@
       </tbody>
     </v-table>
     <v-container class="text-center">
-      <v-btn color="purple darken-2" dark class="mb-5" @click="$router.push('/')">
-        Return to the Realm
-      </v-btn>
+      <v-btn color="secondary" class="mb-5" @click="$router.push('/')"
+        >Home</v-btn
+      >
     </v-container>
   </v-card>
 </template>
@@ -41,43 +41,38 @@ interface Player {
   averageAttempts: number;
 }
 
-// const topTenScores = ref<Player[]>([]);
-// if (process.server && process.static) {
-//   // Use mock data during prerendering if necessary
-//   topTenScores.value = [
-//     // Provide mock data here
-//   ];
-// } else {
-//   Axios.get('/leaderboard/GetScores')
-//     .then(response => {
-//       topTenScores.value = response.data;
-//     })
-//     .catch(error => {
-//       console.error('Failed to load scores:', error);
-//       topTenScores.value = [];
-//     });
-// }
+const topTenScores = ref<Player[]>();
+
+//need to make GET request to the server to get the leaderboard data
+Axios.get('/leaderboard/GetScores')
+  .then(response => {
+    topTenScores.value = response.data;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+//note to self need to implement animate.css for the leaderboard extra credit
 </script>
-
-<style scoped>
-.v-table {
-  background-color: #663399;
-  color: #fff;
-  border-radius: 8px;
+<style>
+.table {
+  background-color: unknownGradient;
+  animation: colorChange 10s infinite;
 }
-
-.v-card {
-  background-image: url('/images/lexiquest_background.jpg');
-  background-size: cover;
-  background-repeat: no-repeat;
-}
-
-.v-table thead th {
-  font-family: 'Merriweather', serif; 
-  text-shadow: 1px 1px 2px black;
-}
-
-.v-btn {
-  font-family: 'Merriweather', serif;
+@keyframes colorChange {
+  0% {
+    background-color: #b7afaf;
+  }
+  25% {
+    background-color: #ffd54f;
+  }
+  50% {
+    background-color: #9575cd;
+  }
+  75% {
+    background-color: #482e74;
+  }
+  100% {
+    background-color: #4f4f4f;
+  }
 }
 </style>
