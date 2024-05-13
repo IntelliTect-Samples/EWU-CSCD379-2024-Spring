d86e4905e0f8f1cc3d01efb48a85f826513f86cb
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-12 18:55:18 -0700

    Update WordleDbContext.cs

--- Wordle.Api/Wordle.Api/Models/WordleDbContext.cs
+++ Wordle.Api/Wordle.Api/Models/WordleDbContext.cs
@@ -8,6 +8,7 @@ namespace Wordle.Api.Models
         public DbSet<WordOfTheDay> WordsOfTheDays { get; set; }
         public DbSet<Game> Games { get; set; }
         public DbSet<Word> Words { get; set; }
+        public DbSet<Player> Players { get; set; }
         
         public WordleDbContext(DbContextOptions<WordleDbContext> options)
             : base(options)
