30c90382506a5442d7fcf2535148895ba70b5136
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-12 18:26:09 -0700

    Create player.cs

---
+++ Wordle.Api/Wordle.Api/Models/player.cs
@@ -0,0 +1,8 @@
+public class Player
+{
+    public int PlayerId { get; set; }
+    public string Name { get; set; }
+    public int GameCount { get; set; }
+    public double AverageAttempts { get; set; }
+    public int? AverageSecondsPerGame { get; set; } // Optional
+}
