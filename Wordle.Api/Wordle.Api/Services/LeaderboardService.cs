f79172030c43b1d06ec2831790744b22af98a01f
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-12 19:48:12 -0700

    Update LeaderboardService.cs

--- Wordle.Api/Wordle.Api/Services/LeaderboardService.cs
+++ Wordle.Api/Wordle.Api/Services/LeaderboardService.cs
@@ -1,62 +1,35 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Mvc;
 using Wordle.Api.Services;
 using Wordle.Api.Dtos;
 using Wordle.Api.Models;
 
-namespace Wordle.Api.Services;
+namespace Wordle.Api.Controllers;
 
-public class LeaderboardService
+[ApiController]
+[Route("[controller]")]
+public class LeaderboardController : ControllerBase
 {
-    private readonly WordleDbContext _context;
-    private static readonly object _changingPlayerLock = new();
-    private static readonly object _addingPlayerLock = new();
+	private readonly LeaderboardService _service;
+	public LeaderboardController(LeaderboardService service)
+	{
+		_service = service;
+	}
 
-    public LeaderboardService(WordleDbContext context)
-    {
-        _context = context;
-    }
+	[HttpGet("GetScores")]
+	public async Task<List<PlayerDto>> Get()
+	{
+		return await _service.GetTopScoresAsync();
+	}
 
-    public async Task<List<PlayerDto>> GetTopScoresAsync()
-    {
-        return await _context.Players
-            .OrderBy(p => p.AverageAttempts)
-            .Take(10)
-            .Select(p => new PlayerDto
-            {
-                Name = p.Name,
-                GameCount = p.GameCount,
-                AverageAttempts = p.AverageAttempts
-            }).ToListAsync();
-    }
-
-    public async Task<Player> PostScoreAsync(PlayerDto request)
-    {
-        var foundPlayer = await _context.Players.FirstOrDefaultAsync(p => p.Name == request.Name);
-        if (foundComposer != null)
-        {
-            lock (_changingPlayerLock)
-            {
-                double attempts = foundPlayer.AverageAttempts * foundPlayer.GameCount + request.AverageAttempts;
-                foundPlayer.GameCount += request.GameGameCount;
-                foundPlayer.AverageAttempts = attempts / foundPlayer.GameCount;
-                await _context.SaveChangesAsync();
-                return foundPlayer;
-            }
-        }
-        else
-        {
-            lock (_addingPlayerLock)
-            {
-                var newPlayer = new Player
-                {
-                    Name = request.Name,
-                    AverageAttempts = request.AverageAttempts,
-                    GameCount = request.GameCount,
-                };
-                _context.Players.Add(newPlayer);
-                await _context.SaveChangesAsync();
-                return newPlayer;
-            }
-        }
-    }
+	[HttpPost("PostScore")]
+	public async Task<PlayerDto> Post(PlayerDto request)
+	{
+		Player player = await _service.PostScoreAsync(request);
+		return new PlayerDto
+		{
+			Name = player.Name,
+			GameCount = player.GameCount,
+			AverageAttempts = player.AverageAttempts
+		};
+	}
 }
