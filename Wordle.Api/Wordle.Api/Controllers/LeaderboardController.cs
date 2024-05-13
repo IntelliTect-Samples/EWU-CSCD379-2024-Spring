b23c2b34616411025dd8ddc5693e7b415aadc101
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-12 19:32:21 -0700

    Update LeaderboardController.cs

--- Wordle.Api/Wordle.Api/Controllers/LeaderboardController.cs
+++ Wordle.Api/Wordle.Api/Controllers/LeaderboardController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Wordle.Api.Services;
 using Wordle.Api.Dtos;
 using Wordle.Api.Models;
@@ -9,27 +9,28 @@ namespace Wordle.Api.Controllers;
 [Route("[controller]")]
 public class LeaderboardController : ControllerBase
 {
-	private readonly LeaderboardService _service;
-	public LeaderboardController(LeaderboardService service)
-	{
-		_service = service;
-	}
+    private readonly LeaderboardService _service;
 
-	[HttpGet("GetScores")]
-	public async Task<List<PlayerDto>> Get()
-	{
-		return await _service.GetTopScoresAsync();
-	}
+    public LeaderboardController(LeaderboardService service)
+    {
+        _service = service;
+    }
 
-	[HttpPost("PostScore")]
-	public async Task<PlayerDto> Post(PlayerDto request)
-	{
-		Player player = await _service.PostScoreAsync(request);
-		return new PlayerDto
-		{
-			Name = player.Name,
-			GameCount = player.GameCount,
-			AverageAttempts = player.AverageAttempts
-		};
-	}
+    [HttpGet("GetScores")]
+    public async Task<ActionResult<List<PlayerDto>>> Get()
+    {
+        return Ok(await _service.GetTopScoresAsync());
+    }
+
+    [HttpPost("PostScore")]
+    public async Task<ActionResult<PlayerDto>> Post(PlayerDto request)
+    {
+        var player = await _service.PostScoreAsync(request);
+        return Ok(new PlayerDto
+        {
+            Name = player.Name,
+            GameCount = player.GameCount,
+            AverageAttempts = player.AverageAttempts
+        });
+    }
 }
