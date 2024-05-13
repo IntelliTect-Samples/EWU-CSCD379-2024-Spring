cb36796ce14b6dbffd7663c66d3f6fc5b0dba3d3
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-12 22:06:35 -0700

    Update Program.cs

--- Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
