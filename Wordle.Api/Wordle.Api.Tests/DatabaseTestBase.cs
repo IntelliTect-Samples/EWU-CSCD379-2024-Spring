ee47287a88be44a5787e98a11e6d404b8c189ffd
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-12 23:54:24 -0700

    Update DatabaseTestBase.cs

--- Wordle.Api/Wordle.Api.Tests/DatabaseTestBase.cs
+++ Wordle.Api/Wordle.Api.Tests/DatabaseTestBase.cs
@@ -1,14 +1,25 @@
+﻿using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Data.Sqlite;
-using Wordle.Api.Data;
-using Wordle.Api.Dtos;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Wordle.Api.Models;
 
 namespace Wordle.Api.Tests;
-
-public abstract class DatabaseTestBase
+public class DatabaseTestBase
 {
     private SqliteConnection SqliteConnection { get; set; } = null!;
     protected DbContextOptions<WordleDbContext> Options { get; private set; } = null!;
+
+    private static ILoggerFactory GetLoggerFactory()
+    {
+        IServiceCollection serviceCollection = new ServiceCollection();
+        serviceCollection.AddLogging(builder =>
+        {
+            builder.AddConsole();
+        });
+        return serviceCollection.BuildServiceProvider().
+            GetService<ILoggerFactory>()!;
+    }
 
     [TestInitialize]
     public void InitializeDb()
@@ -18,16 +29,12 @@ public abstract class DatabaseTestBase
 
         Options = new DbContextOptionsBuilder<WordleDbContext>()
             .UseSqlite(SqliteConnection)
+            .UseLoggerFactory(GetLoggerFactory())
+            .EnableSensitiveDataLogging()
             .Options;
 
         using var context = new WordleDbContext(Options);
         context.Database.EnsureCreated();
-    }
-
-    [TestCleanup]
-    public void CloseDbConnection()
-    {
-        SqliteConnection.Close();
     }
 
     public static IEnumerable<PlayerDto> Requests { get; } = new PlayerDto[] {
@@ -38,4 +45,11 @@ public abstract class DatabaseTestBase
         new PlayerDto { Name = "Thorn Underleaf", AverageAttempts = 1, GameCount = 1 },
         new PlayerDto { Name = "Lilith Darkweaver", AverageAttempts = 2, GameCount = 1 },
     };
+}
+
+    [TestCleanup]
+    public void CloseDbConnection()
+    {
+        SqliteConnection.Close();
+    }
 }
