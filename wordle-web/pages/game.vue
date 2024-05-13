158975177dc4004f858069b5ae013a2878bf2d3f
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-13 00:51:03 -0700

    Update game.vue

--- wordle-web/pages/game.vue
+++ wordle-web/pages/game.vue
@@ -1,108 +1,134 @@
 <template>
   <v-container>
     <v-progress-linear
-      v-if="game.loading"
-      color="deep-purple accent-4"
-      indeterminate
-    />
+      v-if="game.gameState === GameState.Initializing"
+      color="amber darken-2"
+      indeterminate />
     <v-card v-else class="text-center" elevation="2">
-      <v-sheet color="amber darken-3" class="pa-3">
-        <h1 class="display-1 white--text">LexiQuest!</h1>
-        <v-btn @click="toggleNameDialog" color="green accent-4" text>
-          {{ username }}
-        </v-btn>
+      <v-sheet>
+        <v-container height="150px">
+          <v-sheet
+            @click="showNameDialog = !showNameDialog"
+            class="pa-2 mx-2 mt-2 cursor-pointer"
+            color="deep-purple accent-4">
+            {{ username }}
+          </v-sheet>
+        </v-container>
       </v-sheet>
       <v-alert
-        v-if="game.gameState !== GameState.Playing"
-        :color="game.gameState === GameState.Won ? 'green' : 'red'"
-        dense>
+        v-if="game.gameState != GameState.Playing"
+        :color="game.gameState == GameState.Won ? 'green' : 'red'"
+        class="mb-5"
+        tile>
         <h3>
-          {{ game.gameState === GameState.Won ? "Victory! 🧙‍♂️" : "Defeat... 💀" }}
+          You've
+          {{ game.gameState == GameState.Won ? 'triumphed! 🏰' : 'been defeated... 🌑' }}
         </h3>
         <v-card-text>
-          The secret rune was: <strong>{{ game.secretWord }}</strong>
+          The secret word was: <strong>{{ game.secretWord }}</strong>
         </v-card-text>
-        <v-btn outlined color="amber" @click="restartGame">
-          <v-icon left>mdi-restart</v-icon>
-          Start a New Quest
+        <v-btn variant="outlined" @click="game.startNewGame()">
+          <v-icon size="large" class="mr-2"> mdi-restart </v-icon> Restart Quest
         </v-btn>
       </v-alert>
 
-      <GameBoardGuess
-        v-for="(guess, i) in game.guesses"
-        :key="i"
-        :guess="guess" />
+      <div class="mt-5">
+        <GameBoardGuess
+          v-for="(guess, i) of game.guesses"
+          :key="i"
+          :guess="guess" />
+      </div>
 
-      <v-btn @click="submitGuess" color="purple darken-2" dark class="mb-3">
+      <div class="my-10">
+        <Keyboard />
+      </div>
+
+      <v-btn
+        @click="game.submitGuess(); playAudio();"
+        class="mb-5"
+        color="amber darken-2">
         Cast Spell!
       </v-btn>
-      <v-btn @click="toggleLeaderboard" color="primary" dark class="mb-3">
-        View Leaderboard
+      <v-btn
+        @click="$router.push('/leaderboard'); playAudio();"
+        class="mb-5 mx-5"
+        color="deep-purple darken-2">
+        View Hall of Heroes
       </v-btn>
+      <NameDialog
+        v-model:show="showNameDialog"
+        v-model:name="username"
+        @entered="enterName" />
+      <GuestSaveDialog
+        v-model="showGuestSaveDialog"
+        @saveGuestScore="postScore" />
     </v-card>
-    <NameDialog v-model:show="showNameDialog" v-model:name="username" />
   </v-container>
 </template>
 
-<script setup>
-import { ref, reactive, onMounted, onUnmounted, watch } from 'vue';
-import { Game, GameState } from '../scripts/game';
-import NameDialog from '../components/NameDialog';
+<script setup lang="ts">
+import { Game, GameState } from '~/scripts/game';
 import nuxtStorage from 'nuxt-storage';
 import Axios from 'axios';
-import { useRouter } from 'vue-router';
+
 const game = reactive(new Game());
-const router = useRouter();
+game.startNewGame();
+provide('GAME', game);
+const username = ref('');
 const showNameDialog = ref(false);
-const username = ref(nuxtStorage.localStorage.getData('name') || 'Guest');
-provide("GAME", game);
-function submitGuess() {
-  game.submitGuess();
-  if (game.gameState === GameState.Won || game.gameState === GameState.Lost) {
-    postScore();
+const showGuestSaveDialog = ref(false);
+
+function playAudio(): void {
+  const audio = new Audio('/magic-spell.mp3');
+  audio.volume = 0.8;
+  audio.play();
+}
+
+onMounted(() => {
+  window.addEventListener('keyup', onKeyup);
+  const defaultName = nuxtStorage.localStorage.getData('name');
+  showNameDialog.value = !defaultName;
+  username.value = defaultName || 'Guest';
+});
+
+onUnmounted(() => {
+  window.removeEventListener('keyup', onKeyup);
+});
+
+watch(() => game.gameState, () => {
+  if (game.gameState == GameState.Won || game.gameState == GameState.Lost) {
+    showGuestSaveDialog.value = username.value === 'Guest';
+    if (!showGuestSaveDialog.value) {
+      postScore();
+    }
+  }
+});
+
+function onKeyup(event: KeyboardEvent) {
+  if (showNameDialog.value) {
+    if (event.key === 'Enter') {
+      enterName();
+    }
+  } else {
+    game.handleKey(event);
   }
 }
-function toggleLeaderboard() {
-  router.push('/leaderboard');
-}
-function restartGame() {
-  game.startNewGame();
-}
-function toggleNameDialog() {
-  showNameDialog.value = !showNameDialog.value;
-}
+
 function enterName() {
-  username.value = username.value.trim() || 'Guest';
-  nuxtStorage.localStorage.setData('name', username.value);
+  if (username.value.trim() === '') {
+    username.value = 'Guest';
+  } else {
+    nuxtStorage.localStorage.setData('name', username.value);
+  }
   showNameDialog.value = false;
 }
+
 function postScore() {
-  const attempts = game.guessIndex + 1; 
+  const attempts = game.gameState == GameState.Won ? game.guessIndex + 1 : game.maxAttempts;
   Axios.post('/leaderboard/postscore', {
     Name: username.value,
     GameCount: 1,
     AverageAttempts: attempts,
   });
 }
-onMounted(() => {
-  window.addEventListener("keyup", onKeyup);
-	@@ -59,13 +101,20 @@ onUnmounted(() => {
-  window.removeEventListener("keyup", onKeyup);
-});
-function onKeyup(event) {
-  if (event.key === "Enter") {
-    submitGuess();
-  } else if (event.key === "Backspace") {
-    game.removeLastLetter();
-  } else if (event.key.match(/[A-z]/) && event.key.length === 1) {
-    game.addLetter(event.key.toUpperCase());
-  }
-}
 </script>
-
-<style scoped>
-.v-sheet {
-  background-image: url('/images/lexiquest_background.jpg');
-  background-size: cover;
-}
-</style>
