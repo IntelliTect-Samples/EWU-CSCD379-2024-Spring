09a0ed9feeb94dcbadbac694bc2e8f657dec96e5
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-12 23:48:56 -0700

    Update Player.cs

--- Wordle.Api/Wordle.Api/Models/Player.cs
+++ Wordle.Api/Wordle.Api/Models/Player.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Wordle.Api.Models
@@ -6,10 +6,21 @@ namespace Wordle.Api.Models
     [Table("Players")]
     public class Player
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PlayerId { get; set; }
-        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "Game count must be non-negative.")]
         public int GameCount { get; set; }
+
+        [Range(0.0, double.MaxValue, ErrorMessage = "Average attempts must be non-negative.")]
         public double AverageAttempts { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Average seconds per game must be non-negative.")]
         public int AverageSecondsPerGame { get; set; } // Optional
     }
 }
