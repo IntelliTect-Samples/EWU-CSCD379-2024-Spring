cedc76a5a329ad7ce739f12cd24b8885249da09a
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-14 11:47:30 -0700

    Update NameDialog.vue
    changed to be more fantasy themed

--- wordle-web/components/NameDialog.vue
+++ wordle-web/components/NameDialog.vue
@@ -1,16 +1,19 @@
 <template>
   <v-dialog v-model="showModel" max-width="500" persistent>
     <v-card>
-      <v-sheet color="primary"><v-card-title>Enter Thy Name, Brave Adventurer!</v-card-title></v-sheet>
+      <v-sheet color="primary" class="dialog-header">
+        <v-card-title class="dialog-title">Enter Thy Name, Brave Adventurer!</v-card-title>
+      </v-sheet>
       <v-form>
         <v-responsive hide-details="auto" width="500">
           <v-text-field
             append-inner-icon="mdi-sword-cross"
             label="Name"
             v-model="nameModel"
-            @keydown.enter
+            class="name-input"
+            @keydown.enter.prevent
             @click:append-inner="$emit('entered')"
-            @keydown.enter.prevent></v-text-field>
+          ></v-text-field>
         </v-responsive>
       </v-form>
     </v-card>
@@ -18,11 +21,31 @@
 </template>
 
 <script setup lang="ts">
-
 const showModel = defineModel<boolean>('show');
 const nameModel = defineModel<string>('name');
-  
 defineEmits(['entered']);
-  
 </script>
+
+<style scoped>
+.dialog-header {
+  background-image: url('path/to/your/fantasy-background.jpg'); /* Add a fantasy-themed background image */
+  background-size: cover;
+  text-align: center;
+}
+
+.dialog-title {
+  font-family: 'MedievalSharp', serif; /* Use a fantasy-themed font */
+  color: #fff;
+}
+
+.name-input {
+  --v-text-field-label-color: #fff; /* Make label text white */
+  --v-input-background: rgba(0, 0, 0, 0.5); /* Add a semi-transparent background to the input */
+  --v-input-color: #fff; /* Make input text color white */
+}
+
+.name-input .v-input__icon--append {
+  color: #fff;
+}
+</style>
 
