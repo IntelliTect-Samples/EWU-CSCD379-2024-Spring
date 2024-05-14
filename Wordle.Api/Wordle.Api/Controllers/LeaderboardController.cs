54c7237ebd49d40c6413c0f707a6f90c24e8817b
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-14 12:00:22 -0700

    Update LeaderboardController.cs

--- Wordle.Api/Wordle.Api/Controllers/LeaderboardController.cs
+++ Wordle.Api/Wordle.Api/Controllers/LeaderboardController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Wordle.Api.Services;
 using Wordle.Api.Dtos;
 using Wordle.Api.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Wordle.Api.Controllers;
 
@@ -9,27 +11,43 @@ namespace Wordle.Api.Controllers;
 [Route("[controller]")]
 public class LeaderboardController : ControllerBase
 {
-	private readonly LeaderboardService _service;
-	public LeaderboardController(LeaderboardService service)
-	{
-		_service = service;
-	}
+    private readonly LeaderboardService _service;
 
-	[HttpGet("GetScores")]
-	public async Task<List<PlayerDto>> Get()
-	{
-		return await _service.GetTopScoresAsync();
-	}
+    public LeaderboardController(LeaderboardService service)
+    {
+        _service = service;
+    }
 
-	[HttpPost("PostScore")]
-	public async Task<PlayerDto> Post(PlayerDto request)
-	{
-		Player player = await _service.PostScoreAsync(request);
-		return new PlayerDto
-		{
-			Name = player.Name,
-			GameCount = player.GameCount,
-			AverageAttempts = player.AverageAttempts
-		};
-	}
+    [HttpGet("GetScores")]
+    public async Task<ActionResult<List<PlayerDto>>> Get()
+    {
+        try
+        {
+            var scores = await _service.GetTopScoresAsync();
+            return Ok(scores);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { message = ex.Message });
+        }
+    }
+
+    [HttpPost("PostScore")]
+    public async Task<ActionResult<PlayerDto>> Post(PlayerDto request)
+    {
+        try
+        {
+            Player player = await _service.PostScoreAsync(request);
+            return Ok(new PlayerDto
+            {
+                Name = player.Name,
+                GameCount = player.GameCount,
+                AverageAttempts = player.AverageAttempts
+            });
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new { message = ex.Message });
+        }
+    }
 }
