2e042b58e6f9b34a159a16177699ca3560a85ed4
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-14 12:00:03 -0700

    Update LeaderboardService.cs

--- Wordle.Api/Wordle.Api/Services/LeaderboardService.cs
+++ Wordle.Api/Wordle.Api/Services/LeaderboardService.cs
@@ -1,76 +1,58 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Wordle.Api.Data;
 using Wordle.Api.Dtos;
 using Wordle.Api.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Wordle.Api.Services;
 
 public class LeaderboardService
 {
-	private readonly AppDbContext _context;
-	private static object _changingPlayerLock = new();
-	private static object _addingPlayerLock = new();
-	public LeaderboardService(AppDbContext context)
-	{
-		_context = context;
-	}
-	public async Task<List<PlayerDto>> GetTopScoresAsync()
-	{
-		return await _context.Players.
-			OrderBy(player => player.AverageAttempts).
-			Take(10).
-			Select(player => new PlayerDto
-			{
-				Name = player.Name,
-				GameCount = player.GameCount,
-				AverageAttempts = player.AverageAttempts
-			}).ToListAsync();
-	}
+    private readonly AppDbContext _context;
 
-	public async Task<Player> PostScoreAsync(PlayerDto request)
-	{
-		Player? foundPlayer = await _context.Players.FirstOrDefaultAsync(player => player.Name.Equals(request.Name));
-		if (foundPlayer is not null)
-		{
-			lock (_changingPlayerLock)
-			{
-				double attempts = foundPlayer.AverageAttempts * foundPlayer.GameCount + request.AverageAttempts;
-				foundPlayer.GameCount += request.GameCount;
-				foundPlayer.AverageAttempts = attempts / foundPlayer.GameCount;
-				_context.SaveChanges();
-				return foundPlayer;
-			}
-		}
-		else
-		{
-			lock (_addingPlayerLock)
-			{
-				foundPlayer = _context.Players.FirstOrDefault(player => player.Name.Equals(request.Name));
-				if (foundPlayer is not null)
-				{
-					double attempts = foundPlayer.AverageAttempts * foundPlayer.GameCount + request.AverageAttempts;
-					foundPlayer.GameCount += request.GameCount;
-					foundPlayer.AverageAttempts = attempts / foundPlayer.GameCount;
-					_context.SaveChanges();
-					return foundPlayer;
-				}
-				else
-				{
-					Player addedPlayer = new()
-					{
-						AverageAttempts = request.AverageAttempts,
-						GameCount = request.GameCount,
-						Name = request.Name,
-					};
-					_context.Players.Add(addedPlayer);
-					_context.SaveChanges();
-					return addedPlayer;
+    public LeaderboardService(AppDbContext context)
+    {
+        _context = context;
+    }
 
-				}
+    public async Task<List<PlayerDto>> GetTopScoresAsync()
+    {
+        return await _context.Players
+            .OrderBy(player => player.AverageAttempts)
+            .Take(10)
+            .Select(player => new PlayerDto
+            {
+                Name = player.Name,
+                GameCount = player.GameCount,
+                AverageAttempts = player.AverageAttempts
+            }).ToListAsync();
+    }
 
-			}
-		}
-	}
+    public async Task<Player> PostScoreAsync(PlayerDto request)
+    {
+        var foundPlayer = await _context.Players.FirstOrDefaultAsync(player => player.Name.Equals(request.Name));
 
+        if (foundPlayer != null)
+        {
+            double attempts = foundPlayer.AverageAttempts * foundPlayer.GameCount + request.AverageAttempts;
+            foundPlayer.GameCount += request.GameCount;
+            foundPlayer.AverageAttempts = attempts / foundPlayer.GameCount;
+            _context.Players.Update(foundPlayer);
+        }
+        else
+        {
+            foundPlayer = new Player
+            {
+                Name = request.Name,
+                GameCount = request.GameCount,
+                AverageAttempts = request.AverageAttempts
+            };
+            await _context.Players.AddAsync(foundPlayer);
+        }
 
+        await _context.SaveChangesAsync();
+        return foundPlayer;
+    }
 }
