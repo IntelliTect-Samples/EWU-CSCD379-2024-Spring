8bb3a8dfc7d113e0c64a9f8eb19f93e94b797d06
Joshua Lester <jlester3@ewu.edu>
2024-05-18 12:45:49 -0700

    Testing axios mock in GitHub actions

--- wordle-web/tests/game.test.ts
+++ wordle-web/tests/game.test.ts
@@ -1,19 +1,34 @@
 // @vitest-environment nuxt
-import { test, expect } from 'vitest';
+import { beforeEach, test, expect } from 'vitest';
 import { LetterState } from '~/scripts/letter';
 import { Game, GameOption } from '~/scripts/game';
+import AxiosMockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import { GameStats } from '~/scripts/gameStats';
+
+let mock: AxiosMockAdapter;
+
+beforeEach(() => {
+  mock = new AxiosMockAdapter(axios);
+});
 
 test('game', () => {
   // create game and check if it's created
   const game = new Game(GameOption.SelectedWord);
-  game.postScores = false;
   game.startNewGame('autos');
   expect(game.secretWord.length).toBe(5);
 });
 
 test('guess-word', () => {
+  const mockGameStats = new GameStats();
+  mockGameStats.word = 'autos';
+  mockGameStats.averageGuesses = 0;
+  mockGameStats.totalTimesPlayed = 0;
+  mockGameStats.totalWins = 0;
+  mockGameStats.totalLosses = 0;
+
+  mock.onPost().reply(200, mockGameStats);
   const game = new Game(GameOption.SelectedWord);
-  game.postScores = false;
   game.startNewGame('autos');
   game.setGuessLetters('tangs');
   game.submitGuess();
