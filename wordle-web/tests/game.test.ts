63f092eafedb908d3d2616718a9cdee83afa5f90
Meghan <Meg.Woodford@IntelliTect.com>
2024-05-18 10:58:49 -0700

    Fix npm tests

--- wordle-web/tests/game.test.ts
+++ wordle-web/tests/game.test.ts
@@ -1,7 +1,16 @@
 // @vitest-environment nuxt
-import { expect, test } from "vitest";
+import { beforeEach, expect, test } from "vitest";
 import { Game } from "~/scripts/game";
 import { LetterState } from "~/scripts/letter";
+import AxiosMockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { GameStats } from "~/scripts/gameStats";
+
+let mock: AxiosMockAdapter;
+
+beforeEach(() => {
+  mock = new AxiosMockAdapter(axios);
+});
 
 test("game", () => {
   // create game and check if it's created
@@ -12,7 +21,19 @@ test("game", () => {
 
 test("guess-word", () => {
   const game = new Game();
-  game.startNewGame("autos");
+
+  const word = "autos";
+
+  const mockGameStats = new GameStats();
+  mockGameStats.word = "autos";
+  mockGameStats.averageGuesses = 0;
+  mockGameStats.totalTimesPlayed = 0;
+  mockGameStats.totalWins = 0;
+  mockGameStats.totalLosses = 0;
+
+  mock.onPost().reply(200, mockGameStats);
+
+  game.startNewGame(word);
   game.setGuessLetters("tangs");
   game.submitGuess();
   expect(game.guesses[0].letters[0].state).toBe(LetterState.Misplaced);
@@ -21,7 +42,7 @@ test("guess-word", () => {
   expect(game.guesses[0].letters[3].state).toBe(LetterState.Wrong);
   expect(game.guesses[0].letters[4].state).toBe(LetterState.Correct);
 
-  game.setGuessLetters("autos");
+  game.setGuessLetters(word);
   game.submitGuess();
   expect(game.guesses[1].letters[0].state).toBe(LetterState.Correct);
   expect(game.guesses[1].letters[1].state).toBe(LetterState.Correct);
