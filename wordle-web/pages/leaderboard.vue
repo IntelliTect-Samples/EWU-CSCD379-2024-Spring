eeb21a8f683a3be4d0bbd4f95bf6be3cc3495ca8
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-05-19 19:48:40 -0700

    fake data for now :(

--- wordle-web/pages/leaderboard.vue
+++ wordle-web/pages/leaderboard.vue
@@ -1,41 +1,46 @@
 <template>
-  <v-container class="text-center">
-    <v-card class="mx-auto" max-width="1000" outlined>
-      <v-card-title class="text-h5">LEADERBOARD</v-card-title>
-      <v-card-text>
-        <v-row>
-          <v-col cols="1" class="text-center">
-            ID
-          </v-col>
-          <v-col cols="6 " class="text-center">
-            NAME 
-          </v-col>
-          <v-col cols="2" class="">
-            AVG ATTEMPTS
-          </v-col>
-          <v-col cols="2" class="text-center" >
-            PLAYS 
-          </v-col>
-        </v-row>
-        <v-divider></v-divider>
-        <v-table>
+  <div>
+    <v-card class="mx-auto" max-width="1000" outlined color="primary">
+      <v-sheet>
+        <h1 style="font-size: 2em"class="text-center">LeaderBoard🥇👨‍🌾</h1>     
+      </v-sheet>
+      <v-card-text style="font-size: 1em"class="text-center">
+      Here are our top players : 
+  </v-card-text>
+  <v-table>
           <tbody>
-            <tr v-for="player in topTen" :key="player.name">
-              <td class="text-center">{{ player.Name }}</td>
-              <td class="text-center">{{ player.GameCount }}</td>
-              <td class="text-center">{{ player.AverageAttempts }}</td>
+            <tr>
+              <td class="text-center">ID</td>
+              <td class="text-center">Name</td>
+              <td class="text-center">Average Attempts</td>
+              <td class="text-center">Plays</td>
             </tr>
-          </tbody>
-        </v-table>
-      </v-card-text>
+
+            <tr v-for="(row, index) in tableData" :key="index">
+              <td class="text-center">{{ row.Name }}</td>
+              <td class="text-center">{{ row.GameCount }}</td>
+              <td class="text-center">{{ row.AverageAttempts }}</td>
+              <td class="text-center">{{ row.Plays }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+
     </v-card>
-  </v-container>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import Axios from 'axios';
 
+const tableData = ref([
+  { Name: 'user1', GameCount: 24, AverageAttempts: 80, Plays: 30},
+  { Name: 'oompaloompa', GameCount: 24, AverageAttempts: 80, Plays: 10},
+  { Name: 'ultimatehillbill', GameCount: 24, AverageAttempts: 80, Plays: 3},
+  { Name: 'billyhilly', GameCount: 24, AverageAttempts: 80, Plays: 400},
+  { Name: 'thehillbillmaster', GameCount: 24, AverageAttempts: 80, Plays: 60},
+
+])
 interface PlayerInfo{
   name: string;
   gameCount: number;
