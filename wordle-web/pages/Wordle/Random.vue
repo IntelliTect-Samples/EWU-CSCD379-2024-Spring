b5eaaa26008102c8988cd82b44a1ede4a262554f
alexa darrington <adarrington@ewu.edu>
2024-05-21 16:12:37 -0700

    WIP on Assignment4

--- wordle-web/pages/Wordle/Random.vue
+++ wordle-web/pages/Wordle/Random.vue
@@ -7,7 +7,7 @@
     />
     <v-sheet v-else color="transparent">
       <v-card-title
-        class="text-h6 text-center text-primary font-weight-bold mb-3"
+        class="font-text text-h6 text-center text-wrap text-white font-weight-bold mb-3"
       >
         Random Wordle</v-card-title
       >
@@ -299,3 +299,12 @@ function onKeyup(event: KeyboardEvent) {
   }
 }
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+
+.font-text {
+  font-size: 1.4rem;
+  font-family: "Press Start 2P", sans-serif;
+}
+</style>
