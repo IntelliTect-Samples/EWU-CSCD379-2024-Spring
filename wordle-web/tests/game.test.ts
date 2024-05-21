53cf6700642083960214c6edc57aeb0e95a64217
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-05-21 02:18:48 -0700

    remove invalid param from startNewGame

--- wordle-web/tests/game.test.ts
+++ wordle-web/tests/game.test.ts
@@ -2,12 +2,20 @@
 import { beforeEach, expect, test } from "vitest";
 import { Game } from "~/scripts/game";
 import { LetterState } from "~/scripts/letter";
+import AxiosMockAdapter from "axios-mock-adapter";
+import axios from "axios";
 import { GameStats } from "~/scripts/gameStats";
+
+let mock: AxiosMockAdapter;
+
+beforeEach(() => {
+  mock = new AxiosMockAdapter(axios);
+});
 
 test("game", () => {
   // create game and check if it's created
   const game = new Game();
-  game.startNewGame("autos");
+  game.startNewGame();
   expect(game.secretWord.length).toBe(5);
 });
 
