beb4e92d861044294d051a3b20aac2a1401f36bc
Sergei Ashikhmin <sashikhmin@ewu.edu>
2024-05-23 08:57:04 -0700

    refactor: Add icons to side bar

--- wordle-app/app.vue
+++ wordle-app/app.vue
@@ -3,38 +3,45 @@
     <v-app :style="gradientBaseStyle" class="full-page">
       <v-app-bar color="primary" :elevation="2">
         <v-app-bar-title @click="$router.push('/')" style="cursor: pointer">
-          <v-img
-            src="./public/image-Background-Removed.png"
-            alt="Logo"
-            max-width="180"
-            max-height="95"
-          ></v-img>
-      </v-app-bar-title>
-        
+          <v-img src="./public/image-Background-Removed.png" alt="Logo" max-width="180" max-height="95"></v-img>
+        </v-app-bar-title>
+
         <v-btn icon="mdi-cog" @click="showSettingsDialog = true" />
         <v-btn icon="mdi-help-circle" @click="showHelpDialog = true" />
         <HelpDialog v-model="showHelpDialog" />
-        <v-btn @click="toggleDrawer" icon = "mdi-menu"></v-btn>
+        <v-btn @click="toggleDrawer" icon="mdi-menu"></v-btn>
       </v-app-bar>
-      <v-navigation-drawer v-model="drawer"  location="right" disable-resize-watcher>
+      <v-navigation-drawer v-model="drawer" location="right" disable-resize-watcher>
         <v-list>
-          <v-list-item @click="router.push('/About')" class="text-center">About</v-list-item>
+          <v-list-item @click="router.push('/About')" class="text-center">
+            <v-icon class="mr-2">mdi-information</v-icon>
+            About
+          </v-list-item>
         </v-list>
         <v-list>
-          <v-list-item @click="router.push('/Leaderboard')" class="text-center">Leaderboard</v-list-item>
+          <v-list-item @click="router.push('/Leaderboard')" class="text-center">
+            <v-icon class="mr-2">mdi-trophy</v-icon>
+            Leaderboard
+          </v-list-item>
         </v-list>
         <v-list>
-          <v-list-item @click="router.push('/Instructions')" class="text-center">Instructions</v-list-item>
+          <v-list-item @click="router.push('/Instructions')" class="text-center">
+            <v-icon class="mr-2">mdi-book-open</v-icon>
+            Instructions
+          </v-list-item>
         </v-list>
         <v-list>
-          <v-list-item @click="router.push('/Wordhistory')" class="text-center">Word History</v-list-item>
+          <v-list-item @click="router.push('/Wordhistory')" class="text-center">
+            <v-icon class="mr-2">mdi-history</v-icon>
+            Word History
+          </v-list-item>
         </v-list>
       </v-navigation-drawer>
       <v-main>
         <NuxtPage />
       </v-main>
     </v-app>
-    
+
   </NuxtLayout>
   <SettingsDialog v-model="showSettingsDialog" />
 </template>
@@ -76,20 +83,26 @@ watchEffect(() => {
 </script>
 
 <style scoped>
-.full-page {
+.full-page
+{
   height: 100%;
   width: 100%;
   background-size: 200% 200%;
   animation: gradientShift 15s ease infinite;
 }
 
-@keyframes gradientShift {
-  0%, 100% {
+@keyframes gradientShift
+{
+
+  0%,
+  100%
+  {
     background-position: 0% 50%;
   }
-  50% {
+
+  50%
+  {
     background-position: 100% 50%;
   }
 }
-
 </style>
