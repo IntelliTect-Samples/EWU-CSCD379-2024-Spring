a29013eb2510c1afd539ce9db00c010e0353faba
Bryan Snyder <bsnyder13@ewu.edu>
2024-05-22 21:03:36 -0700

    Update Program.cs

--- Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Program.cs
@@ -1,8 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Wordle.Api.Data;
-using Wordle.Api.Models;
-using Wordle.Api.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
