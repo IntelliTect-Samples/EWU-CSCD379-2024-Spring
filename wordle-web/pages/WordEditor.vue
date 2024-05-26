06502791c4d2879fe578313985f78fb45f4860e8
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-05-25 20:39:19 -0700

    sitched to data-table-server

--- wordle-web/pages/WordEditor.vue
+++ wordle-web/pages/WordEditor.vue
@@ -1,24 +1,38 @@
 <template>
-  <v-data-table
+  <v-data-table-server
     :headers="[
-      { key: 'word', value: 'word' },
-      { key: 'isCommon', value: 'isCommon' },
-      { key: 'actions', value: 'actions' },
+      { title: 'Word', key: 'word', value: 'word' },
+      { title: ' is Common', key: 'isCommon', value: 'isCommon' },
+      { title: 'Actions', key: 'actions', value: 'actions' },
     ]"
     :items="wordsList"
+    :loading="isWordsListLoading"
+    :itemsLength="wordsList.length"
     :sort-by="[{ key: 'word', order: 'asc' }]"
+    items-per-page="10"
   >
     <template v-slot:top>
       <v-toolbar flat>
-        <v-toolbar-title>My CRUD</v-toolbar-title>
+        <v-toolbar-title>Word Editor</v-toolbar-title>
         <v-divider class="mx-4" inset vertical></v-divider>
         <v-spacer></v-spacer>
       </v-toolbar>
+      <v-autocomplete
+        v-model="selectedWord"
+        :items="wordsList.map((word) => word.word)"
+        item-text="word"
+        item-value="word"
+        label="Search for a word"
+        hide-no-data
+        hide-details
+        dense
+        clearable
+      />
     </template>
     <template v-slot:item.actions="{ item }"
       ><v-btn>Common</v-btn><v-btn>Delete</v-btn>
     </template>
-  </v-data-table>
+  </v-data-table-server>
 </template>
 
 <script setup lang="ts">
@@ -27,6 +41,7 @@ import Axios from "axios";
 
 const wordsList: Ref<WordDto[]> = ref([]);
 const isWordsListLoading = ref(true);
+const selectedWord = ref<string | null>(null);
 
 onMounted(async () => {
   wordsList.value = await Axios.get("word/wordsList")
