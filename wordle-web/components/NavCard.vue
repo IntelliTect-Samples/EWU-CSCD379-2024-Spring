b0a6887329703124a6badb596e41e2bae4efb6c4
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-05-25 19:51:17 -0700

    added nifty descriptions for each game mode

--- wordle-web/components/NavCard.vue
+++ wordle-web/components/NavCard.vue
@@ -2,13 +2,27 @@
   <v-card
     :color="color"
     :to="`/${route}`"
-    class="glow-btn d-flex justify-center align-center flex-column pa-3"
+    class="glow-btn d-flex pa-3"
     hover
     ripple
     rounded
   >
-    <v-icon size="x-large" :icon="icon" />
-    <v-card-title class="font-weight-bold">{{ title }}</v-card-title>
+    <v-row>
+      <v-col
+        lg="3"
+        md="3"
+        sm="12"
+        class="d-flex align-center justify-center flex-column"
+      >
+        <v-icon size="x-large" :icon="icon" />
+        <v-card-title class="font-weight-bold">{{ title }}</v-card-title>
+      </v-col>
+      <v-col cols="9" v-if="$vuetify.display.mdAndUp">
+        <v-card-text>
+          {{ description }}
+        </v-card-text>
+      </v-col>
+    </v-row>
   </v-card>
 </template>
 
@@ -19,6 +33,7 @@ const props = defineProps<{
   title: string;
   icon: string;
   color: string;
+  description: string;
 }>();
 
 const route = props.title.replace(" ", "");
--- wordle-web/pages/index.vue
+++ wordle-web/pages/index.vue
@@ -39,16 +39,28 @@
       </v-col>
       <v-col lg="6" md="12">
         <v-col>
-          <NavCard title="Word Editor" icon="mdi-pencil" color="primary" />
+          <NavCard
+            title="Word Editor"
+            icon="mdi-pencil"
+            color="primary"
+            description="Refine your artistic vision with our intuitive word editor, where every stroke of your keyboard transforms into a brushstroke on the canvas of imagination. Craft, refine, and perfect your word creations, shaping them into exquisite masterpieces that captivate hearts and minds alike."
+          />
         </v-col>
         <v-col>
-          <NavCard title="Leaderboard" icon="mdi-trophy" color="secondary" />
+          <NavCard
+            title="Leaderboard"
+            icon="mdi-trophy"
+            color="secondary"
+            description="
+Ascend the ranks of word mastery on our leaderboard page, where wordsmiths compete for the top spots based on their creative flair and linguistic finesse. Join the community of wordsmiths and see where you stand among the elite in the realm of aesthetic expression."
+          />
         </v-col>
         <v-col>
           <NavCard
             title="Instructions"
             icon="mdi-help-circle"
             color="primary"
+            description="Unlock the secrets to crafting captivating Wordles with our comprehensive instructions page. Dive into a world of tips, tricks, and techniques, guiding you on a journey to unleash your creativity and master the art of aesthetic wordplay."
           />
         </v-col>
         <v-col>
@@ -56,10 +68,17 @@
             title="Latest Wordles"
             icon="mdi-newspaper"
             color="secondary"
+            description="Embark on a journey through recent wordly wonders with our Replay and Stats section, where the last ten Wordles await your exploration. Delve into the trends and triumphs of the day's players, gaining insights into strategies and celebrating the diversity of linguistic prowess that colors each round."
           />
         </v-col>
         <v-col>
-          <NavCard title="About" icon="mdi-information" color="primary" />
+          <NavCard
+            title="About"
+            icon="mdi-information"
+            color="primary"
+            description="
+Discover the essence of beauty through our curated collection of Wordles, where art and language intertwine seamlessly. Immerse yourself in a realm where each word becomes a stroke of visual poetry, inviting you to explore the boundless dimensions of aesthetics."
+          />
         </v-col>
       </v-col>
     </v-row>
