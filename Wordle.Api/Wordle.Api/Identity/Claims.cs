07085d121c1b01663b2e93134336fd67547e7d3a
alexa darrington <adarrington@ewu.edu>
2024-05-26 16:09:32 -0700

    Added "MasterOfTheUniverse" claim

--- Wordle.Api/Wordle.Api/Identity/Claims.cs
+++ Wordle.Api/Wordle.Api/Identity/Claims.cs
@@ -4,5 +4,6 @@ public static class Claims
 {
     public const string Random = "Random";
     public const string BirthDate = "BirthDate";
+    public const string MasterOfTheUniverse = "MasterOfTheUniverse";
 }
 
