bf8d32f2d61c1722951dab39084109a1fd42e38f
alexa darrington <adarrington@ewu.edu>
2024-05-26 21:24:32 -0700

    Added policy to program.cs

--- Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Program.cs
@@ -96,7 +96,7 @@ builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy(Policies.RandomAdmin, Policies.RandomAdminPolicy);
     //options.AddPolicy("IsGrantPolicy", policy => policy.RequireRole("Grant"));
-    //options.AddPolicy(Policies.EditWord, Policies.EditWordPolicy);
+    options.AddPolicy(Policies.EditWord, Policies.EditWordPolicy);
 });
 
 
