61c24e9155ce0fac57326e754df1efc5ce9fec93
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-05-26 23:12:39 -0700

    styling Word Editor page

--- wordle-web/pages/WordEditor.vue
+++ wordle-web/pages/WordEditor.vue
@@ -1,38 +1,73 @@
 <template>
-  <v-data-table-server
-    :headers="[
-      { title: 'Word', key: 'word', value: 'word' },
-      { title: ' is Common', key: 'isCommon', value: 'isCommon' },
-      { title: 'Actions', key: 'actions', value: 'actions' },
-    ]"
-    :items="wordsList"
-    :loading="isWordsListLoading"
-    :itemsLength="wordsList.length"
-    :sort-by="[{ key: 'word', order: 'asc' }]"
-    items-per-page="10"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>Word Editor</v-toolbar-title>
-        <v-divider class="mx-4" inset vertical></v-divider>
-        <v-spacer></v-spacer>
-      </v-toolbar>
-      <v-autocomplete
-        v-model="selectedWord"
-        :items="wordsList.map((word) => word.word)"
-        item-text="word"
-        item-value="word"
-        label="Search for a word"
-        hide-no-data
-        hide-details
-        dense
-        clearable
-      />
-    </template>
-    <template v-slot:item.actions="{ item }"
-      ><v-btn>Common</v-btn><v-btn>Delete</v-btn>
-    </template>
-  </v-data-table-server>
+  <v-container>
+    <v-card class="pa-3">
+      <v-card-title class="font-weight-bold">Word Editor</v-card-title>
+      <v-card-item>
+        <v-row>
+          <v-col cols="auto">
+            <v-btn color="play">Add Word</v-btn>
+          </v-col>
+          <v-col cols="auto">
+            <v-row>
+              <v-checkbox
+                v-for="(item, index) in items"
+                :key="index"
+                :label="item.label"
+                :value="item.value"
+                v-model="isCommon"
+                direction="horizontal"
+                compact
+                color="play"
+              />
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-card-item>
+
+      <v-data-table
+        :headers="[
+          { title: 'Word', key: 'word', value: 'word' },
+          { title: 'Common Word', key: 'isCommon', value: 'isCommon' },
+          { title: 'Actions', key: 'actions', value: 'actions' },
+        ]"
+        :items="wordsList"
+        :loading="isWordsListLoading"
+        :itemsLength="wordsList.length"
+        :sort-by="[{ key: 'word', order: 'asc' }]"
+        items-per-page="10"
+        :header-props="{ class: 'text-uppercase' }"
+        :cell-props="{ class: 'text-uppercase text-button pa-3' }"
+        show-select
+        :search="selectedWord?.toString() ?? undefined"
+      >
+        <template v-slot:top>
+          <v-text-field
+            v-model="selectedWord"
+            :items="wordsList.map((word) => word.word)"
+            item-text="word"
+            item-value="word"
+            label="Search for a word"
+            hide-no-data
+            hide-details
+            dense
+            clearable
+            single-line
+          />
+        </template>
+        <template v-slot:item.isCommon="{ item }">
+          <div class="d-flex flex-row ga-3">
+            <v-icon :icon="item.isCommon ? 'mdi-check' : 'mdi-close'" />
+          </div>
+        </template>
+        <template v-slot:item.actions="{ item }">
+          <div class="d-flex flex-row ga-3">
+            <v-btn color="win">Common</v-btn>
+            <v-btn color="lose">Delete</v-btn>
+          </div>
+        </template>
+      </v-data-table>
+    </v-card>
+  </v-container>
 </template>
 
 <script setup lang="ts">
@@ -43,6 +78,17 @@ const wordsList: Ref<WordDto[]> = ref([]);
 const isWordsListLoading = ref(true);
 const selectedWord = ref<string | null>(null);
 
+useHead({
+  title: "Word Editor",
+  meta: [{ name: "description", content: "Cool site!" }],
+});
+
+const items = [
+  { label: "Common", value: "common" },
+  { label: "Uncommon", value: "uncommon" },
+];
+
+const isCommon = ref<string | null>(null);
 onMounted(async () => {
   wordsList.value = await Axios.get("word/wordsList")
     .then((res: { data: any }) => res.data)
