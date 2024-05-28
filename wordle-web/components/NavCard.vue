9f6a749f06c743382bfdf1815a92b04fa0fecd97
Johanne McClanahan <jmcclenahan@ewu.edu>
2024-05-27 19:11:30 -0700

    more fixes to responsiveness of gird system in landing page

--- wordle-web/components/NavCard.vue
+++ wordle-web/components/NavCard.vue
@@ -9,9 +9,9 @@
   >
     <v-row :no-gutters="true">
       <v-col
-        cols="3"
-        md="3"
-        sm="12"
+        lg="3"
+        sm="3"
+        xs="12"
         class="d-flex align-center justify-center flex-column"
       >
         <v-icon size="x-large" :icon="icon" />
@@ -19,7 +19,7 @@
           title
         }}</v-card-title>
       </v-col>
-      <v-col cols="9" v-if="$vuetify.display.mdAndUp">
+      <v-col cols="9" v-if="$vuetify.display.smAndUp">
         <v-card-text class="text-left">
           {{ description }}
         </v-card-text>
