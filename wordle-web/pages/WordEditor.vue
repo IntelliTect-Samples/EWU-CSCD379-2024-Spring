53c3d1138f053a395d1b4509c1a862953a379927
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-05-30 16:49:42 -0700

    extra button-centering

--- wordle-web/pages/WordEditor.vue
+++ wordle-web/pages/WordEditor.vue
@@ -7,10 +7,29 @@ Duplicate words should not be allowed ❌✅
 Add & support changing the common word boolean flag ❌✅
 Filter the list of returned words by common words ❌✅
 Words should be sorted alphabetically ❌✅ -->
-
-
-  Editing Words
-
+  <v-container>
+    <v-card class="pa-3">
+      <v-card-title class="font-weight-bold text-center" >Word Editor</v-card-title>
+    </v-card>
+    <v-card-item>
+        <v-row>
+          <v-col cols="auto">
+            <v-btn color="primary">
+              Add Word
+            </v-btn>
+          </v-col>
+          <v-col cols="auto">
+          </v-col>
+          <v-col cols="auto">
+            <v-btn color="primary">
+              EXTRA BUTTON FOR NOW
+            </v-btn>
+          </v-col>
+          <v-col cols="auto">
+          </v-col>
+        </v-row>
+    </v-card-item>
+  </v-container>
 
 
   </div>
