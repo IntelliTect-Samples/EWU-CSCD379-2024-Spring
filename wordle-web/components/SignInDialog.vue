5a65cd78f0c218de1bfcdfeca1f45c9f9cbed474
madams41 <madams41@ewu.edu>
2024-06-03 09:01:14 -0700

    wrote calls for make common and add word

--- wordle-web/components/SignInDialog.vue
+++ wordle-web/components/SignInDialog.vue
@@ -1,59 +1,75 @@
 <template>
-    <v-dialog v-model="modelValue" width="400">
-      <v-card>
-        <v-alert v-if="errorMessage" type="error" title-date-format="Function">
-          {{ errorMessage }}
-        </v-alert>
-        <v-card-text>
-          <v-text-field v-model="userName" @keyup.stop label="Username" />
-          <v-text-field
-            v-model="password"
-            @keyup.stop
-            label="Password"
-            :type="showPassword ? 'text' : 'password'"
-            :append-inner-icon="
-              showPassword ? 'mdi-eye-off-outline' : 'mdi-eye-outline'
-            "
-            @click:append-inner="showPassword = !showPassword"
-          />
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" variant="tonal" @click="modelValue = false">
-            Cancel
-          </v-btn>
-          <v-btn color="primary" variant="flat" @click="signIn"> Sign In </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </template>
-  
-  <script setup lang="ts">
-  import axios from "axios";
-  import TokenService from "@/scripts/tokenService";
-  
-  const tokenService = new TokenService();
-  
-  const modelValue = defineModel<boolean>({ default: false });
-  const showPassword = ref(false);
-  const userName = ref("");
-  const password = ref("");
-  const errorMessage = ref("");
-  
-  function signIn() {
-    errorMessage.value = "";
-    axios
-      .post("/Token/GetToken", {
-        username: userName.value,
-        password: password.value,
-      })
-      .then((response) => {
-        tokenService.setToken(response.data.token);
-        modelValue.value = false;
-      })
-      .catch((error) => {
-        errorMessage.value = error.response.data;
-      });
+  <v-dialog v-model="modelValue" width="400">
+    <v-card>
+      <v-alert v-if="errorMessage" type="error" title-date-format="Function">
+        {{ errorMessage }}
+      </v-alert>
+      <v-card-text>
+        <v-text-field v-model="userName" @keyup.stop label="Username" />
+        <v-text-field
+          v-model="password"
+          @keyup.stop
+          label="Password"
+          :type="showPassword ? 'text' : 'password'"
+          :append-inner-icon="
+            showPassword ? 'mdi-eye-off-outline' : 'mdi-eye-outline'
+          "
+          @click:append-inner="showPassword = !showPassword"
+        />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="primary" variant="tonal" @click="modelValue = false">
+          Cancel
+        </v-btn>
+
+        <v-btn v-if="signedIn" color="primary" variant="flat" @click="signOut">
+          Sign Out
+        </v-btn>
+
+        <v-btn v-if="!signedIn" color="primary" variant="flat" @click="signIn">
+          Sign In
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+import axios from "axios";
+import TokenService from "@/scripts/tokenService";
+const router = useRouter();
+const tokenService = new TokenService();
+const signedIn = computed(() => tokenService.isLoggedIn());
+const modelValue = defineModel<boolean>({ default: false });
+const showPassword = ref(false);
+const userName = ref("");
+const password = ref("");
+const errorMessage = ref("");
+const userNameGlobal: Ref<string> = inject("userName")! as Ref<string>;
+provide("userNameSignIn", userName);
+
+function signIn() {
+  errorMessage.value = "";
+  axios
+    .post("/Token/GetToken", {
+      username: userName.value,
+      password: password.value,
+    })
+    .then((response) => {
+      tokenService.setToken(response.data.token);
       modelValue.value = false;
-  }
-  </script>
+    })
+    .catch((error) => {
+      errorMessage.value = error.response.data;
+    });
+  modelValue.value = false;
+  userNameGlobal.value = userName.value;
+  router.push("/");
+}
+function signOut() {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  router.push("/");
+}
+</script>
