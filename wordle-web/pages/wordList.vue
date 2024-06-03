4f66ffb91c7826af63ae2f845e39c0740a1af468
madams41 <madams41@ewu.edu>
2024-06-03 00:21:36 -0700

    list now populates

--- wordle-web/pages/wordList.vue
+++ wordle-web/pages/wordList.vue
@@ -5,40 +5,58 @@
 
     <v-tooltip text="Sign in to access more features">
       <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" color ="primary" @click="showSignInDialog = true">Sign In</v-btn>
+        <v-btn v-bind="props" color="primary" @click="showSignInDialog = true"
+          >Sign In</v-btn
+        >
       </template>
     </v-tooltip>
-
+    <!---->
     <v-table>
-      <v-table>
-            <thead>
-                <tr>
-                    <th>Word</th>
-                    <th>Is It A Common Word</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="word in wordList" :key="word.word">
-                    <td v-if="word.word">{{ word.word }}</td> <td v-else>No Data</td>
-                    <td v-if="word.isCommon">{{ word.isCommon? "yes": "no" }}</td> <td v-else>No Data</td>
-                    
-                </tr>
-            </tbody>
-        </v-table>
+      <thead>
+        <tr>
+          <th>Word</th>
+          <th>Is It A Common Word</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="words in wordList" :key = words.word>
+          <td v-if="words.word">{{ words.word }}</td>
+          <td v-else>No Data</td>
+          <td v-if="words.isCommon">{{ words.isCommon? "Yes" : "no"}}</td>
+          <td v-else>No Data</td>
+        </tr>
+      </tbody>
     </v-table>
-
-    <v-text-field clearable label ="Search" v-model ="wordToSearch" ></v-text-field>
+    <!---->
+    <v-text-field
+      clearable
+      label="Search"
+      v-model="wordToSearch"
+    ></v-text-field>
     <v-tooltip text="mark the word as a common word">
       <template v-slot:activator="{ props }">
-        <v-btn :disabled="!signedIn" color ="primary"v-bind="props">Common</v-btn>
+        <v-btn :disabled="!signedIn" color="primary" v-bind="props"
+          >Common</v-btn
+        >
       </template>
     </v-tooltip>
-
+    <!---->
     <div class="d-flex">
-      <v-text-field clearable label ="Add A Word" v-model = wordToAdd @click =" addWord()"></v-text-field>
+      <v-text-field
+        clearable
+        label="Add A Word"
+        v-model="wordToAdd"
+        @click="addWord()"
+      ></v-text-field>
       <v-tooltip text="Add a word to the word list">
         <template v-slot:activator="{ props }">
-          <v-btn :disabled="!canAddWords" color ="primary" v-bind="props" @click ="console.log('word to add ' + wordToAdd)">Add Word</v-btn>
+          <v-btn
+            :disabled="!canAddWords"
+            color="primary"
+            v-bind="props"
+            @click="console.log('word to add ' + wordToAdd)"
+            >Add Word</v-btn
+          >
         </template>
       </v-tooltip>
     </div>
@@ -47,11 +65,14 @@
 <script setup lang="ts">
 import TokenService from "~/scripts/tokenService";
 import Axios from "axios";
+import { Word } from "~/scripts/word";
 const showSignInDialog = ref(false);
 const tokenService = new TokenService();
 const wordToAdd = ref("");
 const wordToSearch = ref("");
-const wordList = ref<WordListItem[]>();
+const wordListApi = ref<WordListApi[]>();
+const wordList = ref<WordListItems[]>();
+const totalCount = ref(0);
 const pageNumber = ref(1);
 const pageSize = ref(10);
 const signedIn = computed(() => tokenService.isLoggedIn());
@@ -63,16 +84,19 @@ const canAddWords = computed(() => {
     return false;
   }
 });
-
-interface WordListItem {
+interface WordListApi {
+  count: number;
+  items: WordListItems[];
+}
+interface WordListItems {
   word: string;
   isCommon: boolean;
 }
-onMounted(() => {
+onMounted(async () => {
   if (tokenService.isLoggedIn()) {
     showSignInDialog.value = false;
   }
-  getWordList();
+  wordList.value = await getWordList();
 });
 function addWord() {
   console.log("word to add " + wordToAdd.value);
@@ -82,15 +106,21 @@ function addWord() {
   });
 }
 async function getWordList() {
-  let wordUrl = "Word/WordsList";
-  console.log("get wordlist from api " + wordUrl + "?page=" + pageNumber.value + "&pageSize=" + pageSize.value);
-  await Axios.get(wordUrl + "?page=" + pageNumber.value + "&pageSize=" + pageSize.value)
-    .then((response) => {
-      wordList.value = response.data;
-      console.log("api get words response " + response.status);
+  let items: WordListItems[] = [];
+  await Axios.get(
+    `word/WordsList?query=${wordToSearch.value}&page=${pageNumber.value}&pageSize=${pageSize.value}`
+  )
+  .then((res) => res.data)
+    .then((data: any) => {
+      items = data["items"].map((data: any) => ({
+        word: data.word,
+        isCommon: data.isCommonWord,
+      }));
+      totalCount.value = data["count"];
     })
     .catch((error) => {
       console.log("api get Words error " + error);
     });
+    return items;
 }
 </script>
