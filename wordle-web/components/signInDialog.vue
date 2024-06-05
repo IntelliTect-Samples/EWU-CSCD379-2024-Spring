77b4e880926031ae6f7c80bc63486d9197805106
Cynthia Montalvo <cmontalvo@ewu.edu>
2024-06-04 18:10:34 -0700

    adding password visibility

--- wordle-web/components/signInDialog.vue
+++ wordle-web/components/signInDialog.vue
@@ -2,9 +2,7 @@
   <v-dialog v-model="modelValue" max-width="500">
     <v-card color="secondary">
       <v-sheet color="primary">
-        <v-card-text
-          style="text-align: center; font-size: larger"
-        >
+        <v-card-text style="text-align: center; font-size: larger">
           <header>
             Sign In
           </header>
@@ -21,15 +19,18 @@
           ></v-text-field>
           <v-text-field
             v-model="password"
+            :type="seeWhileTyping ? 'text' : 'password'"
             label="Password"
-            type="password"
             prepend-icon="mdi-lock"
+            :append-inner-icon="seeWhileTyping ? 'mdi-eye-outline' : 'mdi-eye-off-outline'"
+            @click:append-inner="seeWhileTyping = !seeWhileTyping"
             required
           ></v-text-field>
         </v-form>
       </v-card-text>
       <v-card-actions>
-          <v-btn color="primary" @click="submit">Sign In</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" @click="submit">Sign In</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -40,15 +41,13 @@ import { ref } from 'vue';
 
 const modelValue = ref<boolean>(false);
 const username = ref<string>('');
+const seeWhileTyping = ref<boolean>(false);
 const password = ref<string>('');
 const valid = ref<boolean>(false);
 
 const submit = () => {
-
 };
-
 </script>
 
 <style>
-
 </style>
