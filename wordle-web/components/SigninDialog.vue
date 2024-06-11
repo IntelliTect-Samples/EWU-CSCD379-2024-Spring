def9de61b419d88215fef8a979a9b90847adc400
Bryan Snyder <bsnyder13@ewu.edu>
2024-06-11 04:40:02 -0700

    added sign up to login

--- wordle-web/components/SigninDialog.vue
+++ wordle-web/components/SigninDialog.vue
@@ -1,11 +1,12 @@
 <template>
-  <v-dialog v-model="modelValue" width="400">
+  <v-dialog v-model="modelValue" width="500">
     <v-card>
-      <v-alert v-if="errorMessage" type="error" title-date-format="Function">
+      <v-alert v-if="errorMessage" type="error">
         {{ errorMessage }}
       </v-alert>
       <v-card-text>
         <v-text-field v-model="userName" @keyup.stop label="Username" />
+        <v-text-field v-if="isSignUp" v-model="email" @keyup.stop label="Email" />
         <v-text-field
           v-model="password"
           @keyup.stop
@@ -16,17 +17,34 @@
           "
           @click:append-inner="showPassword = !showPassword"
         />
+        <v-text-field v-if="isSignUp" v-model="secretPhrase" @keyup.stop label="Secret Phrase" />
+        <v-date-picker v-if="isSignUp" v-model="birthday" label="Birthday" />
       </v-card-text>
       <v-card-actions>
-        <v-spacer />
-        <v-btn color="primary" variant="tonal" @click="modelValue = false">
-          Cancel
-        </v-btn>
-        <v-btn color="primary" variant="flat" @click="signIn"> Sign In </v-btn>
+        <v-row justify="center">
+          <v-col cols="auto">
+            <v-btn color="primary" variant="tonal" @click="modelValue = false">
+              Cancel
+            </v-btn>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn color="primary" variant="flat" @click="isSignUp ? signUp : signIn">
+              {{ isSignUp ? 'Sign Up' : 'Sign In' }}
+            </v-btn>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn @click="toggleSignUp">
+              {{ isSignUp ? 'Sign In' : 'Sign Up' }}
+            </v-btn>
+          </v-col>
+        </v-row>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
+
+
+
 
 <script setup lang="ts">
 import axios from "axios";
@@ -34,9 +52,14 @@ const modelValue = defineModel<boolean>({ default: false });
 const showPassword = ref(false);
 const userName = ref("");
 const password = ref("");
+const email = ref("");
+const secretPhrase = ref("");
+const birthday = ref(null);
 const errorMessage = ref("");
+const isSignUp = ref(false);
+
 function signIn() {
-    errorMessage.value = "";
+  errorMessage.value = "";
   axios
     .post("/Token/GetToken", {
       username: userName.value,
@@ -48,5 +71,27 @@ function signIn() {
     .catch((error) => {
       errorMessage.value = error.response.data;
     });
+}
+
+function signUp() {
+  errorMessage.value = "";
+  axios
+    .post("/User/Register", {
+      username: userName.value,
+      password: password.value,
+      email: email.value,
+      secretPhrase: secretPhrase.value,
+      birthday: birthday.value,
+    })
+    .then((response) => {
+      modelValue.value = false;
+    })
+    .catch((error) => {
+      errorMessage.value = error.response.data;
+    });
+}
+
+function toggleSignUp() {
+  isSignUp.value = !isSignUp.value;
 }
 </script>
