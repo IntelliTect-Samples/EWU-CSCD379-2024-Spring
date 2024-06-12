da96a9dd014e3830135466071eb1da2bdb67cff7
Bryan Snyder <bsnyder13@ewu.edu>
2024-06-12 04:48:29 -0700

    CORS policy is applied before any authentication and authorization middleware

--- Wordle.Api/Wordle.Api/Program.cs
+++ Wordle.Api/Wordle.Api/Program.cs
@@ -67,7 +67,7 @@ builder.Services.AddAuthentication(options =>
         ValidateIssuerSigningKey = true,
         ValidIssuer = builder.Configuration["Jwt:Issuer"],
         ValidAudience = builder.Configuration["Jwt:Audience"],
-        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? ""))
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? ""))
     };
 });
 
@@ -99,6 +99,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Ensure UseCors is called before UseAuthentication and UseAuthorization
 app.UseCors("AllowAllOrigins");
 
 app.UseAuthentication();
